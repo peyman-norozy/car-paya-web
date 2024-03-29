3cf1b2ce8e429524bdc3d900624559eac6037cf6
peyman norozy <peyman.norozy@gmail.com>
2024-03-29 17:49:39 +0330

    custom next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env:{
+        BASE_API: "https://api.ccme.ir/api/V1",
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "api.ccme.ir",
+            },
+        ],
+    },
+    reactStrictMode:false
+};
 
 export default nextConfig;
