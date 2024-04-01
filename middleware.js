148ff271b4f81ebe1b551234928df79b07f60518
peyman norozy <peyman.norozy@gmail.com>
2024-04-01 22:40:12 +0330

    fixed the middleware problem

--- middleware.js
+++ middleware.js
@@ -1,7 +1,6 @@
 import {NextResponse} from "next/server";
 
 export default function middleware(req) {
-    console.log("Middleware File", req);
     if (
         req.nextUrl.pathname.startsWith("/login") &&
         req.cookies.has("Authorization")
@@ -16,22 +15,5 @@ export default function middleware(req) {
 }
 
 export const config = {
-    matcher: ["/login/:path*", "/profile/:path*"],
+    matcher: ["/:path*","/login/:path*", "/profile/:path*"],
 };
-// import {NextResponse} from "next/server";
-
-// export default function middleware(req, event) {
-//   console.log(req.nextUrl.pathname);
-//   event.waitUntil(
-//     fetch("http://localhost:3000/api/hello", {
-//       method: "GET",
-//     }).then(async (res) => {
-//       res = await res.json();
-//       console.log(res);
-//     }),
-//   );
-//   return NextResponse.next();
-// }
-// export const config = {
-//   matcher: "/api/:path*",
-// };
