e017805e4801af44a6da3b127745f45c57b02c41
peyman norozy <peyman.norozy@gmail.com>
2024-04-03 00:00:22 +0330

    pass data to the HowWorks.jsx component

--- components/HowWorks.jsx
+++ components/HowWorks.jsx
@@ -2,39 +2,7 @@
 import React from "react";
 import Image from "next/image";
 
-const howWorksData = [
-  {
-    title: "انتخاب وسیله نقلیه",
-    description:
-      "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ است. ",
-    img: "/assets/icons/Perfect-Car-Service.png",
-  },
-  {
-    title: "انتخاب خدمات مورد نیاز",
-    description:
-      "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ است. ",
-    img: "/assets/icons/Doorstep-Pick-up.png",
-  },
-  {
-    title: "انتخاب مکان دریافت خدمات",
-    description:
-      "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ است. ",
-    img: "/assets/icons/service-real-time.png",
-  },
-  {
-    title: "انتخاب زمان دریافت خدمات",
-    description:
-      "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ است. ",
-    img: "/assets/icons/Doorstep-Pick-up.png",
-  },
-  {
-    title: "دریافت خدمات",
-    description:
-      "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ است. ",
-    img: "/assets/icons/Earn-While-We-Service.png",
-  },
-];
-const HowWorks = () => {
+const HowWorks = (props) => {
   return (
     <div className="flex size1000:flex-row flex-col items-center gap-16 mt-24">
       <section>
@@ -51,7 +19,7 @@ const HowWorks = () => {
           کار چک می چگونه کار می کند؟
         </h1>
         <ul className="flex flex-col h-[calc(100%-120px)] gap-8 justify-between w-full border-r-2 pr-8">
-          {howWorksData.map((item, index) => (
+          {props.data.map((item, index) => (
             <li
               key={index}
               className="flex items-center size1000:gap-0 gap-4 justify-between size1000:pl-10 pl-0"
