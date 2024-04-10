793a84b0c81d460e93497a55abaf9e97b9ddeb3f
peyman norozy <peyman.norozy@gmail.com>
2024-04-10 12:25:51 +0330

    delete ResponsiveVehicleRegistration.jsx component

--- components/MainBanner.jsx
+++ components/MainBanner.jsx
@@ -2,8 +2,6 @@
 import MainBannerSlider from "@/components/MainBannerSlider";
 import VehicleRegistration from "@/components/VehicleRegistration";
 import {useSelector} from "react-redux";
-import ResponsiveVehicleRegistration from "@/components/cards/ResponsiveVehicleRegistration";
-
 export default function MainBanner(props) {
     const innerWidth = useSelector((width) => width.todo.windowInnerWidth);
     return (
@@ -14,7 +12,6 @@ export default function MainBanner(props) {
                 </div>
                 <MainBannerSlider/>
             </div>
-            {innerWidth < 1000 && <ResponsiveVehicleRegistration/>}
             <div
                 className="bg-[#ffffff] w-full h-fit px-[20px] grid size1275:grid-cols-6 grid-cols-3 gap-4 mt-10">
                 {props.children}
