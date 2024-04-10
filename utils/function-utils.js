90ef4723362d3df317eaf63fb9d59c314810ce3a
peyman norozy <peyman.norozy@gmail.com>
2024-04-11 01:03:31 +0330

    add condition to the numberWithCommas function in function-utils.js

--- utils/function-utils.js
+++ utils/function-utils.js
@@ -44,9 +44,12 @@ function forceOnlyNumberInput(event) {
 }
 
 function numberWithCommas(x) {
-    x = x.toString();
-    const pattern = /(-?\d+)(\d{3})/;
-    while (pattern.test(x)) x = x.replace(pattern, "$1,$2");
+    console.log(x)
+    if(x){
+        x = x.toString();
+        const pattern = /(-?\d+)(\d{3})/;
+        while (pattern.test(x)) x = x.replace(pattern, "$1,$2");
+    }
     return x;
 }
 
