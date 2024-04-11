726bd476d4d972b061b0de19163dc043f160c2c9
peyman norozy <peyman.norozy@gmail.com>
2024-04-11 17:36:25 +0330

    add getData function and getMultipleData function to the client-api-function-utils.js

--- utils/client-api-function-utils.js
+++ utils/client-api-function-utils.js
@@ -3,20 +3,35 @@
 import {service} from "@/service/service";
 import axios from "axios";
 
-export async function postData(apiRoute,fd) {
-    service()
-    try {
-        return await axios.post(apiRoute,fd)
-    }catch (error){
-        return error
-    }
-}
-
 export async function getData(apiRoute) {
     service()
     try {
         return await axios.get(apiRoute)
-    }catch (error){
+    } catch (error) {
+        return error
+    }
+}
+export async function postData(apiRoute, fd) {
+    service()
+    try {
+        return await axios.post(apiRoute, fd)
+    } catch (error) {
+        return error
+    }
+}
+export async function putData(apiRoute, fd, configHeader) {
+    service()
+    try {
+        return await axios.put(apiRoute, fd, {headers: {configHeader}})
+    } catch (error) {
+        return error
+    }
+}
+export async function getMultipleData(apiRoute, fd, configHeader) {
+    service()
+    try {
+        return await axios.put(apiRoute, fd, {headers: {configHeader}})
+    } catch (error) {
         return error
     }
 }
