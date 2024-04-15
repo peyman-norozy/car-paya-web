83128e062e0b6bcc53feb3c1f311e69807536089
peyman norozy <peyman.norozy@gmail.com>
2024-04-15 23:11:03 +0330

    make data.js file

---
+++ staticData/data.js
@@ -0,0 +1,143 @@
+export const MainBannerData = [
+    {
+        title: "کارشناسی خودرو",
+        iconSrc: "/assets/images/vehicle-verification.png",
+        href: "/vehicle-verification",
+        id: "vehicleVerification"
+    },
+    {
+        title: "بیمه",
+        iconSrc: "/assets/images/insurance.png",
+        href: "#",
+        id: "insurance"
+    },
+    {
+        title: "شناسنامه و سوابق خودرو",
+        iconSrc: "/assets/images/car-background.png",
+        href: "/profile/my-vehicle/my-car",
+        id: "myVehicleMyCar"
+    },
+    {
+        title: "سرویس دوره ای",
+        iconSrc: "/assets/images/periodic-service.png",
+        href: "/periodic-service",
+        id: "periodicService"
+    },
+    {
+        title: "فروشگاه باتری",
+        iconSrc: "/assets/images/battery-product.svg",
+        href: "/batteries",
+        id: "batteries"
+    },
+    {
+        title: "فیلتر روغن(لوازم یدکی)",
+        iconSrc: "/assets/images/cluch-bumpers.png",
+        href: "/products",
+        id: "products"
+    },
+    {
+        title: "فیلتر روغن(لوازم یدکی)",
+        iconSrc: "/assets/images/cluch-bumpers.png",
+        href: "/products",
+        id: "products"
+    },
+    {
+        title: "فیلتر روغن(لوازم یدکی)",
+        iconSrc: "/assets/images/cluch-bumpers.png",
+        href: "/products",
+        id: "products"
+    },
+];
+export const HowWorksMockUpData = [
+    {
+        title: "انتخاب وسیله نقلیه",
+        description:
+            "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ است. ",
+        img: "/assets/icons/Perfect-Car-Service.png",
+    },
+    {
+        title: "انتخاب خدمات مورد نیاز",
+        description:
+            "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ است. ",
+        img: "/assets/icons/Doorstep-Pick-up.png",
+    },
+    {
+        title: "انتخاب مکان دریافت خدمات",
+        description:
+            "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ است. ",
+        img: "/assets/icons/service-real-time.png",
+    },
+    {
+        title: "انتخاب زمان دریافت خدمات",
+        description:
+            "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ است. ",
+        img: "/assets/icons/Doorstep-Pick-up.png",
+    },
+    {
+        title: "دریافت خدمات",
+        description:
+            "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ است. ",
+        img: "/assets/icons/Earn-While-We-Service.png",
+    },
+];
+export const CarProductsMockUpData = [
+    {
+        discount: "تخفیف ۲۰٪",
+        imgSrc: "/assets/images/hiclipart.com (2).png",
+        alt: "oil",
+        title: "روغن موتور بوش مدل لاکچری SN حجم ۴ لیتر",
+        newPrice: "",
+        oldPrice: "",
+    },
+    {
+        discount: "تخفیف ۲۰٪",
+        imgSrc: "/assets/images/hiclipart.com (3).png",
+        alt: "oil",
+        title: "روغن موتور بوش مدل لاکچری SN حجم ۴ لیتر",
+        newPrice: "",
+        oldPrice: "",
+    },
+    {
+        discount: "تخفیف ۲۰٪",
+        imgSrc: "/assets/images/hiclipart.com.png",
+        alt: "oil",
+        title: "فیلتر هوا جک 3s 65746",
+        newPrice: "",
+        oldPrice: "",
+    },
+    {
+        discount: "تخفیف ۲۰٪",
+        imgSrc: "/assets/images/hiclipart.com (1).png",
+        alt: "oil",
+        title: "فیلتر هوا جک 3s 65746",
+        newPrice: "",
+        oldPrice: "",
+    },
+    {
+        discount: "تخفیف ۲۰٪",
+        imgSrc: "/assets/images/hiclipart.com (1).png",
+        alt: "oil",
+        title: "فیلتر هوا جک 3s 65746",
+        newPrice: "",
+        oldPrice: "",
+    },
+    {
+        discount: "تخفیف ۲۰٪",
+        imgSrc: "/assets/images/hiclipart.com (1).png",
+        alt: "oil",
+        title: "فیلتر هوا جک 3s 65746",
+        newPrice: "",
+        oldPrice: "",
+    },
+    {
+        discount: "تخفیف ۲۰٪",
+        imgSrc: "/assets/images/hiclipart.com (1).png",
+        alt: "oil",
+        title: "فیلتر هوا جک 3s 65746",
+        newPrice: "",
+        oldPrice: "",
+    },
+];
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {MainBannerData, HowWorksMockUpData}
