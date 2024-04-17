521f75475131213988e7d866b6885da1eb4ec652
peyman norozy <peyman.norozy@gmail.com>
2024-04-18 00:28:28 +0330

    import CarServicesSlider to the PeriodicService page and use them

--- app/periodic-service/page.jsx
+++ app/periodic-service/page.jsx
@@ -1,103 +1,17 @@
-'use client'
 import Image from "next/image";
 import Button from "@/components/Button";
 import PeriodicServiceCard from "@/components/cards/PeriodicServiceCard";
 import Input from "@/components/Input";
-import PeriodicServiceTabCard from "@/components/cards/PeriodicServiceTabCard";
 import PeriodicServiceUnderCard from "@/components/cards/PeriodicServiceUnderCard";
-import {useState} from "react";
-import cluch from "@/public/assets/images/cluch-bumpers.png";
-import battery from "@/public/assets/images/battery-product.svg";
-import service from "@/public/assets/images/periodic-service.png";
-import car_bg from "@/public/assets/images/car-background.png";
-import insurance from "@/public/assets/images/insurance.png";
+import {Suspense} from "react";
+import {serviceData,periodicServiceUnderCardData,workData} from "@/staticData/data";
+import CarServicesSlider from "@/components/CarServicesSlider/CarServicesSlider";
 
-const workData = [
-  {
-    title: "تعویض روغن",
-    price: 3000000,
-    has_come: 1608663149,
-  },
-  {
-    title: "تعویض روغن",
-    price: 3000000,
-    has_come: 1708863149,
-  },
-];
-
-const periodicServiceData = [
-  {
-    title: "کارشناسی خودرو",
-    icon: "/assets/images/cluch-bumpers.png",
-    href: "/vehicle-verification",
-  },
-  {
-    title: "بیمه",
-    icon: insurance,
-    href: "/",
-  },
-  {
-    title: "شناسنامه و سوابق خودرو",
-    icon: car_bg,
-    href: "/profile/my-vehicle/my-car",
-  },
-  {
-    title: "سرویس دوره ای",
-    icon: service,
-    href: "/periodic-service"
-  },
-  {
-    title: "فروشگاه باتری",
-    icon: battery,
-    href: "/batteries"
-  },
-  {
-    title: "فیلتر و روغن (لوازم یدکی)",
-    icon: cluch,
-    href: "/products"
-  },
-
-];
-
-const periodicServiceUnderCardData = [
-  {
-    title: "سرویس دوره ای اقتصادی",
-    titleDescription: "(شامل ۱۷ نوع خدمات)",
-    options: [
-      "تعویض روغن موتور",
-      "تعویض فیلتر هوا",
-      "تعویض فیلتر روغن",
-      "تعویض فیلتر کابین",
-    ],
-  },
-  {
-    title: "سرویس دوره‌ای استاندارد",
-    titleDescription: "(شامل ۱۷ نوع خدمات)",
-    options: [
-      "تعویض روغن موتور",
-      "تعویض فیلتر هوا",
-      "تعویض فیلتر روغن",
-      "تعویض فیلتر کابین",
-    ],
-  },
-  {
-    title: "سرویس دوره‌ای ویژه",
-    titleDescription: "(شامل ۱۷ نوع خدمات)",
-    options: [
-      "تعویض روغن موتور",
-      "تعویض فیلتر هوا",
-      "تعویض فیلتر روغن",
-      "تعویض فیلتر کابین",
-    ],
-  },
-];
+const CarServicesSliderData = async () => {
+  return <CarServicesSlider data={serviceData}/>;
+};
 
 const PeriodicService = () => {
-  const [isClicked, setIsClicked] = useState(3);
-
-  const selectTabHandler = (index) => {
-    setIsClicked(index);
-  };
   return (
     <div className="flex items-start gap-4 m-4 mt-[88px]">
       <div className="w-[360px] p-4 flex flex-col gap-4 shadow-[0_0_6px_0_rgba(177,177,177,1)] rounded-10">
@@ -155,22 +69,10 @@ const PeriodicService = () => {
           </div>
         </section>
       </div>
-      <div className={"flex-1"}>
-        <ul className={"flex items-baseline gap-4"}>
-          {periodicServiceData.map((item, index) => {
-            return <PeriodicServiceTabCard
-                href={item.href}
-                isClicked={isClicked}
-                onClick={() => selectTabHandler(index)}
-                index={index}
-                key={index}
-                title={item.title}
-                src={item.icon}
-                width={60}
-                height={60}
-            />;
-          })}
-        </ul>
+      <div className={"w-[calc(100%-360px)]"}>
+        <Suspense fallback={<div>....Loading</div>}>
+          <CarServicesSliderData/>
+        </Suspense>
         <div className={"mt-14"}>
           <ul className={"flex flex-col gap-4"}>
             {periodicServiceUnderCardData.map((item, index) => (
