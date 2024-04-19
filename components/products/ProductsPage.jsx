0b12e034eb6bc12d57175cf1d8562ed2af695312
peyman norozy <peyman.norozy@gmail.com>
2024-04-19 13:32:07 +0330

    fixed slider in ProductsPage.jsx

--- components/products/ProductsPage.jsx
+++ components/products/ProductsPage.jsx
@@ -9,208 +9,191 @@ import service from '@/public/assets/images/periodic-service.png'
 import car_bg from '@/public/assets/images/car-background.png'
 import verification from "@/public/assets/images/vehicle-verification.png";
 import caspian from "@/public/assets/images/caspian.png";
-import { useState } from "react";
+import React, {useState} from "react";
 import ProductCard from "@/components/cards/ProductCard";
 import CheckFilter from "@/components/CheckFilter";
 import SelectMultipleOptions from "@/components/SelectMultipleOptions";
+import {serviceData} from "@/staticData/data";
+import CarServicesSlider from "@/components/CarServicesSlider/CarServicesSlider";
 
 const ProductsPage = () => {
-  const [isClicked, setIsClicked] = useState(0);
-  const [isSelected,setIsSelected] = useState(null)
-  
-  const tabTitle = [{ name: "خودرو" }, { name: "موتور سیکلت" }];
-  const verificationTab = [
-    { title: "فیلتر و روغن", src: cluch , href : '/‌products' },
+    const [isClicked, setIsClicked] = useState(0);
+    const [isSelected, setIsSelected] = useState(null)
 
-    { title: 'فروشگاه باتری', src: battery , href : '/batteries' },
-    { title: 'سرویس دوره ای', src: service , href : '/periodic-service' },
-    { title: "شناسنامه و سوابق خودرو", src: car_bg , href : "/profile/my-vehicle/my-car" },
-    { title: "بیمه", src: insurance , href: '/' },
-    { title: "کارشناسی خودرو", src: verification , href : '/vehicle-verification' },
-  ];
+    const tabTitle = [{name: "خودرو"}, {name: "موتور سیکلت"}];
 
-  const productData = [
-    {
-      image: caspian,
-      title: "روغن موتور کاسترول",
-      oldPrice: 6000000,
-      newPrice: 6000000,
-      options: [
-        { key: "مدل", value: "G7135153" },
-        { key: "حجم", value: "4 لیتر" },
-        { key: "درجه گرانروی", value: "30 w" },
-        { key: "مدل", value: "G7135153" },
-        { key: "حجم", value: "4 لیتر" },
-      ],
-    },
-    {
-      image: caspian,
-      title: "روغن موتور کاسترول",
-      oldPrice: 6000000,
-      newPrice: 6000000,
-      options: [
-        { key: "مدل", value: "G7135153" },
-        { key: "حجم", value: "4 لیتر" },
-        { key: "درجه گرانروی", value: "30 w" },
-        { key: "مدل", value: "G7135153" },
-        { key: "حجم", value: "4 لیتر" },
-      ],
-    },
-    {
-      image: caspian,
-      title: "روغن موتور کاسترول",
-      oldPrice: 6000000,
-      newPrice: 6000000,
-      options: [
-        { key: "مدل", value: "G7135153" },
-        { key: "حجم", value: "4 لیتر" },
-        { key: "درجه گرانروی", value: "30 w" },
-        { key: "مدل", value: "G7135153" },
-        { key: "حجم", value: "4 لیتر" },
-      ],
-    },
-    {
-      image: caspian,
-      title: "روغن موتور کاسترول",
-      oldPrice: 6000000,
-      newPrice: 6000000,
-      options: [
-        { key: "مدل", value: "G7135153" },
-        { key: "حجم", value: "4 لیتر" },
-        { key: "درجه گرانروی", value: "30 w" },
-        { key: "مدل", value: "G7135153" },
-        { key: "حجم", value: "4 لیتر" },
-      ],
-    },
-    {
-      image: caspian,
-      title: "روغن موتور کاسترول",
-      oldPrice: 6000000,
-      newPrice: 6000000,
-      options: [
-        { key: "مدل", value: "G7135153" },
-        { key: "حجم", value: "4 لیتر" },
-        { key: "درجه گرانروی", value: "30 w" },
-        { key: "مدل", value: "G7135153" },
-        { key: "حجم", value: "4 لیتر" },
-      ],
-    },
-    {
-      image: caspian,
-      title: "روغن موتور کاسترول",
-      oldPrice: 6000000,
-      newPrice: 6000000,
-      options: [
-        { key: "مدل", value: "G7135153" },
-        { key: "حجم", value: "4 لیتر" },
-        { key: "درجه گرانروی", value: "30 w" },
-        { key: "مدل", value: "G7135153" },
-        { key: "حجم", value: "4 لیتر" },
-      ],
-    },
-    {
-      image: caspian,
-      title: "روغن موتور کاسترول",
-      oldPrice: 6000000,
-      newPrice: 6000000,
-      options: [
-        { key: "مدل", value: "G7135153" },
-        { key: "حجم", value: "4 لیتر" },
-        { key: "درجه گرانروی", value: "30 w" },
-        { key: "مدل", value: "G7135153" },
-        { key: "حجم", value: "4 لیتر" },
-      ],
-    },
-    {
-      image: caspian,
-      title: "روغن موتور کاسترول",
-      oldPrice: 6000000,
-      newPrice: 6000000,
-      options: [
-        { key: "مدل", value: "G7135153" },
-        { key: "حجم", value: "4 لیتر" },
-        { key: "درجه گرانروی", value: "30 w" },
-        { key: "مدل", value: "G7135153" },
-        { key: "حجم", value: "4 لیتر" },
-      ],
-    },
-    {
-      image: caspian,
-      title: "روغن موتور کاسترول",
-      oldPrice: 6000000,
-      newPrice: 6000000,
-      options: [
-        { key: "مدل", value: "G7135153" },
-        { key: "حجم", value: "4 لیتر" },
-        { key: "درجه گرانروی", value: "30 w" },
-        { key: "مدل", value: "G7135153" },
-        { key: "حجم", value: "4 لیتر" },
-      ],
-    },
-  ];
+    const productData = [
+        {
+            image: caspian,
+            title: "روغن موتور کاسترول",
+            oldPrice: 6000000,
+            newPrice: 6000000,
+            options: [
+                {key: "مدل", value: "G7135153"},
+                {key: "حجم", value: "4 لیتر"},
+                {key: "درجه گرانروی", value: "30 w"},
+                {key: "مدل", value: "G7135153"},
+                {key: "حجم", value: "4 لیتر"},
+            ],
+        },
+        {
+            image: caspian,
+            title: "روغن موتور کاسترول",
+            oldPrice: 6000000,
+            newPrice: 6000000,
+            options: [
+                {key: "مدل", value: "G7135153"},
+                {key: "حجم", value: "4 لیتر"},
+                {key: "درجه گرانروی", value: "30 w"},
+                {key: "مدل", value: "G7135153"},
+                {key: "حجم", value: "4 لیتر"},
+            ],
+        },
+        {
+            image: caspian,
+            title: "روغن موتور کاسترول",
+            oldPrice: 6000000,
+            newPrice: 6000000,
+            options: [
+                {key: "مدل", value: "G7135153"},
+                {key: "حجم", value: "4 لیتر"},
+                {key: "درجه گرانروی", value: "30 w"},
+                {key: "مدل", value: "G7135153"},
+                {key: "حجم", value: "4 لیتر"},
+            ],
+        },
+        {
+            image: caspian,
+            title: "روغن موتور کاسترول",
+            oldPrice: 6000000,
+            newPrice: 6000000,
+            options: [
+                {key: "مدل", value: "G7135153"},
+                {key: "حجم", value: "4 لیتر"},
+                {key: "درجه گرانروی", value: "30 w"},
+                {key: "مدل", value: "G7135153"},
+                {key: "حجم", value: "4 لیتر"},
+            ],
+        },
+        {
+            image: caspian,
+            title: "روغن موتور کاسترول",
+            oldPrice: 6000000,
+            newPrice: 6000000,
+            options: [
+                {key: "مدل", value: "G7135153"},
+                {key: "حجم", value: "4 لیتر"},
+                {key: "درجه گرانروی", value: "30 w"},
+                {key: "مدل", value: "G7135153"},
+                {key: "حجم", value: "4 لیتر"},
+            ],
+        },
+        {
+            image: caspian,
+            title: "روغن موتور کاسترول",
+            oldPrice: 6000000,
+            newPrice: 6000000,
+            options: [
+                {key: "مدل", value: "G7135153"},
+                {key: "حجم", value: "4 لیتر"},
+                {key: "درجه گرانروی", value: "30 w"},
+                {key: "مدل", value: "G7135153"},
+                {key: "حجم", value: "4 لیتر"},
+            ],
+        },
+        {
+            image: caspian,
+            title: "روغن موتور کاسترول",
+            oldPrice: 6000000,
+            newPrice: 6000000,
+            options: [
+                {key: "مدل", value: "G7135153"},
+                {key: "حجم", value: "4 لیتر"},
+                {key: "درجه گرانروی", value: "30 w"},
+                {key: "مدل", value: "G7135153"},
+                {key: "حجم", value: "4 لیتر"},
+            ],
+        },
+        {
+            image: caspian,
+            title: "روغن موتور کاسترول",
+            oldPrice: 6000000,
+            newPrice: 6000000,
+            options: [
+                {key: "مدل", value: "G7135153"},
+                {key: "حجم", value: "4 لیتر"},
+                {key: "درجه گرانروی", value: "30 w"},
+                {key: "مدل", value: "G7135153"},
+                {key: "حجم", value: "4 لیتر"},
+            ],
+        },
+        {
+            image: caspian,
+            title: "روغن موتور کاسترول",
+            oldPrice: 6000000,
+            newPrice: 6000000,
+            options: [
+                {key: "مدل", value: "G7135153"},
+                {key: "حجم", value: "4 لیتر"},
+                {key: "درجه گرانروی", value: "30 w"},
+                {key: "مدل", value: "G7135153"},
+                {key: "حجم", value: "4 لیتر"},
+            ],
+        },
+    ];
 
-  const selectFilterOptions = [
-    { name: "کالاهای موجود " },
-    { name: "کالاهای تخفیف دار" },
-  ];
+    const selectFilterOptions = [
+        {name: "کالاهای موجود "},
+        {name: "کالاهای تخفیف دار"},
+    ];
 
-  const multipleSelectData = [
-    { name: "لوازم یدکی" },
-    { name: "فیلتر" },
-    { name: "روغن" },
-  ];
+    const multipleSelectData = [
+        {name: "لوازم یدکی"},
+        {name: "فیلتر"},
+        {name: "روغن"},
+    ];
 
-  const selectTabHandler = (index) => {
-    setIsClicked(index);
-  };
-  const selectOptionHandler = (index) => {
-    setIsSelected(index)
-}
-  return (
-    <div className="w-[95%] m-auto flex gap-[4rem]">
-      <div className="w-[45%] mt-[1rem]">
-        <SelectVehicleBox
-          myTehicleTab={0}
-          tabTitle={tabTitle}
-          title="انتخاب وسیله نقلیه"
-        />
-      </div>
-      <div className="w-full mt-[1rem] flex flex-col gap-[1.5rem]">
-        <div className="hidden size1000:flex flex-row-reverse items-center gap-[1rem]">
-          {verificationTab.map((item, index) => (
-            <SelectedVehicleVerificationBox
-            href={item.href}
-              isClicked={isClicked}
-              onClick={() => selectTabHandler(index)}
-              index={index}
-              key={index}
-              title={item.title}
-              src={item.src}
-              width={60}
-              height={60}
-            />
-          ))}
+    const selectTabHandler = (index) => {
+        setIsClicked(index);
+    };
+    const selectOptionHandler = (index) => {
+        setIsSelected(index)
+    }
+    return (
+        <div className="w-[95%] m-auto flex gap-[4rem]">
+            <div className="w-[45%] mt-[1rem]">
+                <SelectVehicleBox
+                    myTehicleTab={0}
+                    tabTitle={tabTitle}
+                    title="انتخاب وسیله نقلیه"
+                />
+            </div>
+            <div className="pt-0 size1056:pt-[9rem] w-full mt-[1rem] flex flex-col gap-[1.5rem]">
+                <div
+                    className="w-[97%] size1056:w-[63%] absolute top-[5.6rem] left-[1.5%] size1090:left-[2.5%] hidden size1000:flex flex-row-reverse items-center gap-[1rem]">
+                    <CarServicesSlider data={serviceData}/>
+                </div>
+                <div className="flex items-center justify-between">
+                    <div className="flex gap-[0.65rem]">
+                        {selectFilterOptions.map((item, index) => (
+                            <CheckFilter key={index} item={item.name}/>
+                        ))}
+                    </div>
+                    <div className="flex gap-[0.65rem]">
+                        {multipleSelectData.map((item, index) => (
+                            <SelectMultipleOptions on_click={() => selectOptionHandler(index)} id={index}
+                                                   name={item.name} isSelected={isSelected} key={index}/>
+                        ))}
+                    </div>
+                </div>
+                <div className="grid grid-cols-3 gap-x-[0.75rem] gap-y-[0.75rem]">
+                    {productData.map((item, index) => (
+                        <ProductCard key={index} data={item}/>
+                    ))}
+                </div>
+            </div>
         </div>
-        <div className="flex items-center justify-between">
-          <div className="flex gap-[0.65rem]">
-            {selectFilterOptions.map((item, index) => (
-              <CheckFilter key={index} item={item.name} />
-            ))}
-          </div>
-          <div className="flex gap-[0.65rem]">
-            {multipleSelectData.map((item, index) => (
-              <SelectMultipleOptions on_click={() => selectOptionHandler(index)} id={index} name={item.name} isSelected={isSelected} key={index} />
-            ))}
-          </div>
-        </div>
-        <div className="grid grid-cols-3 gap-x-[0.75rem] gap-y-[0.75rem]">
-          {productData.map((item, index) => (
-            <ProductCard key={index} data={item} />
-          ))}
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default ProductsPage;
