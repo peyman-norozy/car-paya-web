5d61605c82dc9cb22fe88cd4e0e63cfd83b6314b
peyman norozy <peyman.norozy@gmail.com>
2024-04-21 23:02:14 +0330

    custom PersonalFile.jsx

--- components/PersonalFile.jsx
+++ components/PersonalFile.jsx
@@ -1,8 +1,21 @@
+import ProfileEditeInput from "@/components/ProfileEditeInput/ProfileEditeInput";
+
 const PersonalFile = () => {
   return (
-    <div>
-      <p>personal file</p>
-    </div>
+      <div className={"bg-white flex-1 rounded-[15px] py-8 px-16 shadow-[0_0_6px_0_rgba(177,177,177,1)]"}>
+          <form className={"flex flex-col gap-[43px]"}>
+              <h1 className={"text-center font-bold"}>اطلاعات حساب</h1>
+              <div className={"grid grid-cols-2 gap-[58px]"}>
+                  <ProfileEditeInput icon={"dt-user-o"} title={"نام"}/>
+                  <ProfileEditeInput icon={"dt-user-o"} title={"نام خانوادگی"}/>
+                  <ProfileEditeInput icon={"dt-call-calling-o"} title={"موبایل"}/>
+                  <ProfileEditeInput icon={"dt-call-calling-o"} title={"کد ملی"}/>
+                  <ProfileEditeInput icon={"dt-mail"} title={"ایمیل"}/>
+                  <ProfileEditeInput icon={"dt-gender"} title={"جنسیت"}/>
+              </div>
+              <button type={"submit"} className={"bg-[#354597] w-[150px] h-[40px] self-end mt-[50px] text-[#F6F6F6] rounded-[10px] hover:bg-[#354960]"}>ثبت تغییرات</button>
+          </form>
+      </div>
   );
 };
 
