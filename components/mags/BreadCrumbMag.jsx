de2c64ce4843275f42b43efe47f558dde88fc0ea
Iman Keramati <imankeramati1401@gmail.com>
2024-04-24 01:18:15 +0330

    breadcrumb responsive in mags pages

--- components/mags/BreadCrumbMag.jsx
+++ components/mags/BreadCrumbMag.jsx
@@ -8,12 +8,12 @@ import Link from "next/link";
 const BreadCrumbMag = (props) => {
   return (
     <div className="flex items-center gap-1 mb-8 pt-4">
-        <Link href='/' className="text-14 text-[#999]">صفحه اصلی</Link>
+        <Link href='/' className="text-[0.5rem] size490:text-10 size560:text-12 size690:text-14 text-[#999] line-clamp-1">صفحه اصلی</Link>
       <div className="flex items-center gap-1">
       {props.data.map((item, index) => (
         <Link href={item.url} key={index} className="flex items-center gap-1 text-[#999] last:text-[#000]">
           <Image src={left} alt="" width={20} height={20} />
-          <p className="text-14">{item.name}</p>
+          <p className="text-[0.5rem] size490:text-10 size560:text-12 size690:text-14 line-clamp-1">{item.name}</p>
         </Link>
       ))}
       </div>
