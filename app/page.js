793ef6efaceb1f380e9ca9a9f6d4eb2735c1f1c6
peyman norozy <peyman.norozy@gmail.com>
2024-04-23 22:41:41 +0330

    change icon font file and css file

--- app/page.js
+++ app/page.js
@@ -24,6 +24,7 @@ const ArticleSliderData = async () => {
 export default function Home() {
     return (
         <Fragment>
+            <i className={"icon-Location-Tick-1 text-red-500 text-[100px]"}/>
             <MainBanner>
                 {serviceData.map((item, index) => (
                     <MainBannerCard key={item.title + "-" + index} data={item} index={index}/>
