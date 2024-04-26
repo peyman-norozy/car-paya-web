83cceebbe796303d1866f11643ef0b9890139348
peyman norozy <peyman.norozy@gmail.com>
2024-04-26 14:28:43 +0330

    custom AddressesCard.jsx component

--- components/cards/AddressesCard.jsx
+++ components/cards/AddressesCard.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, {useCallback, useRef, useState} from 'react';
 import Image from "next/image";
+import useClickOutside from "@/hook/useClickOutside";
+import AddressModal from "@/components/modal/AddressModal";
 
 const AddressesCard = () => {
+    const [addressModalState, setAddressModalState] = useState(false)
+    const accontRef = useRef();
+    const close = useCallback(() => setAddressModalState(false), []);
+    useClickOutside(accontRef, close);
+
+    const clickCrudeHandler = () => {
+        setAddressModalState(prevState => !prevState)
+    }
+
+
     return (
         <li className={"flex justify-between shadow-[0_0_6px_0_rgba(200,200,200,1)] p-[16px] rounded-10"}>
             <div>
                 <ul className={"flex flex-col justify-between h-full gap-4 font-light"}>
                     <li className={"flex items-center gap-2"}>
-                       <Image src={"/assets/icons/addressLocation.svg"} alt={"icon"} width={20} height={20}/>
+                        <Image src={"/assets/icons/addressLocation.svg"} alt={"icon"} width={20} height={20}/>
                         <div>
                             <span className={"ml-1"}>آدرس گیرنده:</span>
                             <span>تهران-پاسداران خیابان پایدار فرد کوچه گلستان ۸</span>
@@ -36,11 +48,16 @@ const AddressesCard = () => {
                     </li>
                 </ul>
             </div>
-            <div className={"flex flex-col justify-between"}>
-                <Image src={"/assets/icons/Menu Kebab.svg"} alt={"icon"} width={24} height={24} className={"self-end"}/>
+            <div className={"flex flex-col justify-between relative"}>
+                <Image src={"/assets/icons/Menu Kebab.svg"} alt={"icon"} width={24} height={24}
+                       className={"self-end cursor-pointer"} onClick={clickCrudeHandler} ref={accontRef}/>
                 <Image src={"/assets/images/image 22.png"} alt={"icon"} width={165} height={117} className={"ml-5"}/>
+                {
+                    addressModalState && <AddressModal/>
+                }
             </div>
         </li>
+
     );
 };
 
