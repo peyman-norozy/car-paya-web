20b0403d5d5a17c016861b3da26cb1e17d267dc5
peyman norozy <peyman.norozy@gmail.com>
2024-04-26 14:29:00 +0330

    make AddressModal.jsx component and custom it

---
+++ components/modal/AddressModal.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Image from "next/image";
+
+const AddressModal = () => {
+    return (
+        <div className={"shadow-[0_0_6px_0_rgba(200,200,200,1)] absolute top-[24px] left-[20px] bg-[#fff] rounded-5 flex flex-col justify-center text-14"}>
+            <section className={"flex items-center gap-2 cursor-pointer hover:bg-stone-200 h-[40px] px-[8px]"}>
+                <Image src={"/assets/icons/Edit 2.svg"} alt={"icon"} width={22} height={22}/>
+                <span>ویرایش آدرس</span>
+            </section>
+            <section className={"flex items-center gap-2 cursor-pointer hover:bg-stone-200 h-[40px] px-[8px]"}>
+                <Image src={"/assets/icons/Delete 2.svg"} alt={"icon"} width={22} height={22}/>
+                <span className={"text-[#E73C33]"}>حذف آدرس</span>
+            </section>
+        </div>
+    );
+};
+
+export default AddressModal;
