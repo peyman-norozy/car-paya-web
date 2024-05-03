99ea2229e0497388857f3ab6056c769aea5a875a
peyman norozy <peyman.norozy@gmail.com>
2024-05-03 16:56:50 +0330

    delete dark sudo class from CreateMyCarSkeleton.jsx style

--- components/CreateMyCarSkeleton.jsx
+++ components/CreateMyCarSkeleton.jsx
@@ -3,13 +3,13 @@ import React from "react";
 const CreateMyCarSkeleton = () => {
   return (
     <div role="status" className="max-w-full animate-pulse">
-      <div className="h-16 rounded-10 dark:bg-gray-400 flex justify-between items-center mb-4"></div>
-      <div className="h-16 rounded-10 dark:bg-gray-400 flex justify-between items-center mb-4"></div>
-      <div className="h-16 rounded-10 dark:bg-gray-400 flex justify-between items-center mb-4"></div>
-      <div className="h-16 rounded-10 dark:bg-gray-400 flex justify-between items-center mb-4"></div>
-      <div className="h-16 rounded-10 dark:bg-gray-400 flex justify-between items-center mb-4"></div>
-      <div className="h-16 rounded-10 dark:bg-gray-400 flex justify-between items-center mb-4"></div>
-      <div className="h-16 rounded-10 dark:bg-gray-400 flex justify-between items-center mb-4"></div>
+      <div className="h-16 rounded-10 bg-gray-400 flex justify-between items-center mb-4"></div>
+      <div className="h-16 rounded-10 bg-gray-400 flex justify-between items-center mb-4"></div>
+      <div className="h-16 rounded-10 bg-gray-400 flex justify-between items-center mb-4"></div>
+      <div className="h-16 rounded-10 bg-gray-400 flex justify-between items-center mb-4"></div>
+      <div className="h-16 rounded-10 bg-gray-400 flex justify-between items-center mb-4"></div>
+      <div className="h-16 rounded-10 bg-gray-400 flex justify-between items-center mb-4"></div>
+      <div className="h-16 rounded-10 bg-gray-400 flex justify-between items-center mb-4"></div>
     </div>
   );
 };
