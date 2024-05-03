b9ba0f2a48f1c39ac439320322cb8964286d12a0
Iman Keramati <imankeramati1401@gmail.com>
2024-05-03 14:11:17 +0330

    handle active tab in car slider

--- components/cards/PeriodicServiceTabCard.jsx
+++ components/cards/PeriodicServiceTabCard.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const PeriodicServiceTabCard = (props) => {
   const clickTabHandler = (props) => {};
-  const { src, alt, height, width, title, onClick, isClicked, index, href } =
+  const { src, alt, height, width, title, onClick, isClicked, id, href } =
       props;
   return (
     <li className={"min-w-[200px]"}>
@@ -12,13 +12,13 @@ const PeriodicServiceTabCard = (props) => {
           href={href}
           onClick={onClick}
           className={`w-full flex flex-col items-center justify-center rounded-10 border shadow-[0_5px_15px_0_rgba(0,0,0,0.15)] h-[110px] ${
-              isClicked === index ? "border-RED_500" : "border-[#dedede]"
+              isClicked === href ? "border-RED_500" : "border-[#dedede]"
           }`}
       >
         <Image src={src} alt={alt} height={height} width={width} />
         <p
             className={`text-12 text-center ${
-                isClicked === index ? "text-RED_500" : "text-text_gray"
+                isClicked === href ? "text-RED_500" : "text-text_gray"
             }`}
         >
           {title}
