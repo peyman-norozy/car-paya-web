b01e47d5652e4715453e2b2725ad5e50c5593389
Iman Keramati <imankeramati1401@gmail.com>
2024-05-04 12:27:09 +0330

    showing toastify for saving mag

--- components/mags/MagShowComments.jsx
+++ components/mags/MagShowComments.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
+'use client'
+
+import React, {useEffect} from 'react';
 import MagShowCommentsCard from './MagShowCommentsCard';
+import axios from "axios";
 
 const MagShowComments = () => {
+    useEffect(() => {
+        axios.get(process.env.BASE_API + '/web/mag-comments?mag_id=n7g0yr06' ).then(res => console.log(res)).catch(err => console.log(err))
+    }, []);
     return (
         <div>
             <div className='flex items-center justify-between ml-[2rem] '>
