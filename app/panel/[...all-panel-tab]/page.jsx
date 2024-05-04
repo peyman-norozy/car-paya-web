1db8fc6d840f8746c9ee018def3c604c4b7dd469
peyman norozy <peyman.norozy@gmail.com>
2024-05-04 20:42:33 +0330

    change panelTabData

---
+++ app/panel/[...all-panel-tab]/page.jsx
@@ -0,0 +1,168 @@
+"use client"
+import {useParams, useRouter} from "next/navigation";
+import TitleDescription from "@/components/TitleDescription";
+import {Fragment, useEffect, useState} from "react";
+import LogoutModal from "@/components/modal/LogoutModal";
+import {getWindowInnerWidth} from "@/store/todoSlice";
+import {useDispatch, useSelector} from "react-redux";
+import UserSpecifications from "@/components/UserSpecifications";
+import UserTabsCard from "@/components/cards/UserTabsCard";
+import Image from "next/image";
+import CarDevice from "@/components/CarDevice";
+import MotorDevice from "@/components/MotorDevice";
+import CreateMyCar from "@/components/CreateMyCar";
+import RecordModal from "@/components/modal/RecordModal";
+import RecordModalCreate from "@/components/modal/RecordModalCreate";
+import ProductAddress from "@/components/ProductAddress/ProductAddress";
+import PersonalInformation from "@/components/PersonalInformation";
+
+const panelTabData = [
+  {
+    imgSrc: "/assets/icons/PersonalIcon.svg",
+    title: "داشبورد",
+    alt: "user profile icon",
+    id: "panel",
+  },
+  {
+    imgSrc: "/assets/icons/PersonalIcon.svg",
+    title: "اطلاعات شخصی من",
+    alt: "profile icon",
+    id: "profile",
+  },
+  {
+    imgSrc: "/assets/icons/Group.svg",
+    title: "شناسنامه و سوابق وسیله نقلیه",
+    alt: "my car icon",
+    id: "my-vehicle",
+    children: [
+      { title: "خودرو", id: "my-vehicle/my-car" },
+      { title: "موتور سیکلت", id: "my-vehicle/my-motorcycle" },
+    ],
+  },
+  {
+    imgSrc: "/assets/icons/Vehicle.svg",
+    title: "تاریخچه سفارشات",
+    alt: "destination icon",
+    id: "destination",
+  },
+  {
+    imgSrc: "/assets/icons/location.svg",
+    title: "آدرس های تحویل کالا",
+    alt: "personal file icon",
+    id: "productAddress",
+  },
+  {
+    imgSrc: "/assets/icons/discount-shape.svg",
+    title: "بن ها و امتیازات تخفیف",
+    alt: "loyalty card icon",
+    id: "loyalty-card",
+  },
+];
+
+const AllPanelTab = () => {
+  const [logoutModalState, setLogoutModalState] = useState(false);
+  const router = useRouter();
+  const params = useParams()
+  const dispatch = useDispatch();
+  const RecordModalState = useSelector((state) => state.todo.modalState);
+  const RecordModalCreateState = useSelector(
+    (state) => state.todo.recordModalCreateState,
+  );
+
+  const backClickHandler = () => {
+    if (params) {
+      router.back();
+    } else {
+      router.push("/panel");
+    }
+    // router.back()
+  };
+
+  useEffect(() => {
+    if (params["all-panel-tab"] === "logout") {
+      router.push("my-vehicle", undefined, { scroll: false });
+    }
+  }, [router, params]);
+
+  useEffect(() => {
+    dispatch(getWindowInnerWidth(window.innerWidth));
+    const handleWindowResize = () => {
+      dispatch(getWindowInnerWidth(window.innerWidth));
+    };
+    window.addEventListener("resize", handleWindowResize);
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, [dispatch]);
+
+  return (
+    <Fragment>
+      <div className="mt-[50px] mb-[100px] size460:mx-[50px] mx-[30px]">
+        <div className="flex items-center gap-4 mb-[30px]">
+          <Image
+            src={"/assets/icons/back.svg"}
+            className={"size1000:hidden block cursor-pointer"}
+            onClick={backClickHandler}
+            alt="back icon"
+            width={34}
+            height={34}
+          />
+          {
+            {
+              "my-vehicle/my-car": (
+                <TitleDescription>شناسنامه و سوابق خودرو</TitleDescription>
+              ),
+              "my-vehicle/my-motorcycle": (
+                <TitleDescription>شناسنامه و سوابق موتور</TitleDescription>
+              ),
+              destination: <TitleDescription>تاریخچه سفارشات</TitleDescription>,
+              "productAddress": (
+                <TitleDescription>آدرس های تحویل کالا</TitleDescription>
+              ),
+              "loyalty-card": (
+                <TitleDescription>بن ها و امتیازات تخفیف</TitleDescription>
+              ),
+            }[
+              params["all-panel-tab"] &&
+                params["all-panel-tab"].join("/")
+            ]
+          }
+        </div>
+        <div className="flex gap-4 size1000:flex-row flex-col items-start">
+          <div className="shadow-[0_0_6px_0_rgba(177,177,177,1)] w-[342px] flex-col justify-center gap-4 items-center pt-2 size1000:flex hidden rounded-[10px]">
+            <UserSpecifications
+              style={"flex-col justify-center items-center gap-2"}
+            />
+            <UserTabsCard
+              data={panelTabData}
+              setLogoutModalState={setLogoutModalState}
+            />
+          </div>
+          {
+            {
+              "profile":<PersonalInformation/>,
+              "my-vehicle/my-car": <CreateMyCar />,
+              "my-vehicle/my-car/create": <CarDevice pageType={"create"} />,
+              "my-vehicle/my-car/edit": <CarDevice pageType={"edit"} />,
+              "my-vehicle/my-motorcycle": <MotorDevice />,
+              destination: <div>asdfs</div>,
+              "productAddress": <ProductAddress/>,
+              "user-profile": <div>user-profile</div>,
+              "loyalty-card": <div>loyalty-card</div>,
+            }[
+              params["all-panel-tab"] &&
+                params["all-panel-tab"].join("/")
+            ]
+          }
+        </div>
+        {RecordModalState && <RecordModal />}
+        {RecordModalCreateState && <RecordModalCreate />}
+      </div>
+      {logoutModalState && (
+        <LogoutModal setLogoutModalState={setLogoutModalState} />
+      )}
+    </Fragment>
+  );
+};
+
+export default AllPanelTab;
---
+++ app/panel/page.jsx
@@ -0,0 +1,131 @@
+"use client"
+import { useRouter } from "next/navigation";
+import TitleDescription from "@/components/TitleDescription";
+import { useState } from "react";
+import PersonalFile from "@/components/PersonalFile";
+import { useDispatch, useSelector } from "react-redux";
+import UserSpecifications from "@/components/UserSpecifications";
+import UserTabsCard from "@/components/cards/UserTabsCard";
+import LogoutModal from "@/components/modal/LogoutModal";
+import {setTriangleState} from "@/store/todoSlice";
+
+const panelTabData = [
+  {
+    imgSrc: "/assets/icons/PersonalIcon.svg",
+    title: "داشبورد",
+    alt: "user profile icon",
+    id: "panel",
+  },
+  {
+    imgSrc: "/assets/icons/PersonalIcon.svg",
+    title: "اطلاعات شخصی من",
+    alt: "profile icon",
+    id: "profile",
+  },
+  {
+    imgSrc: "/assets/icons/Group.svg",
+    title: "شناسنامه و سوابق وسیله نقلیه",
+    alt: "my car icon",
+    id: "my-vehicle",
+    children: [
+      { title: "خودرو", id: "my-vehicle/my-car" },
+      { title: "موتور سیکلت", id: "my-vehicle/my-motorcycle" },
+    ],
+  },
+  {
+    imgSrc: "/assets/icons/Vehicle.svg",
+    title: "تاریخچه سفارشات",
+    alt: "destination icon",
+    id: "destination",
+  },
+  {
+    imgSrc: "/assets/icons/location.svg",
+    title: "آدرس های تحویل کالا",
+    alt: "product address file icon",
+    id: "productAddress",
+  },
+  {
+    imgSrc: "/assets/icons/discount-shape.svg",
+    title: "بن ها و امتیازات تخفیف",
+    alt: "loyalty card icon",
+    id: "loyalty-card",
+  },
+];
+
+const UserPanel = () => {
+  const [logoutModalState, setLogoutModalState] = useState(false);
+
+  const dispatch = useDispatch();
+
+  const innerWidthNumber = useSelector(
+    (number) => number.todo.windowInnerWidth,
+  );
+
+  const triangleState = useSelector((state) => state.todo.triangleState);
+  const router = useRouter();
+
+  const tabClickHandler = (event) => {
+    const id = event.currentTarget.getAttribute("tab_id");
+
+    if (id === "my-vehicle") {
+      dispatch(setTriangleState(!triangleState));
+    }
+
+    if (id === "logout") {
+      setLogoutModalState(true);
+    } else if (id === "panel") {
+      router.push("/" + id, undefined, { scroll: false });
+    } else if (id !== "my-vehicle") {
+      router.push("/panel" + "/" + id, undefined, { scroll: false });
+    }
+  };
+
+  return (
+    <div className="mt-[50px] mb-[100px] mx-[50px]">
+      <div className="mb-[30px]">
+        <TitleDescription>اطلاعات شخصی من</TitleDescription>
+      </div>
+      {innerWidthNumber > 1000 ? (
+        <div className="flex gap-4 items-start">
+          <div className="shadow-[0_0_6px_0_rgba(177,177,177,1)] w-[342px] flex flex-col justify-center gap-4 items-center pt-2 rounded-[10px]">
+            <UserSpecifications
+              style={"flex-col justify-center items-center gap-2"}
+            />
+            <UserTabsCard
+              data={panelTabData}
+              triangleState={triangleState}
+              setLogoutModalState={setLogoutModalState}
+              onclick={tabClickHandler}
+            />
+          </div>
+          <PersonalFile />
+          {/*<div role="status" className="animate-pulse flex flex-col gap-8 flex-1">*/}
+          {/*  <div className="h-8 bg-gray-200 rounded-lg dark:bg-gray-400 mb-2.5"></div>*/}
+          {/*  <div className="h-8 bg-gray-200 rounded-lg dark:bg-gray-400 mb-2.5"></div>*/}
+          {/*  <div className="h-8 bg-gray-200 rounded-lg dark:bg-gray-400 mb-2.5"></div>*/}
+          {/*  <div className="h-8 bg-gray-200 rounded-lg dark:bg-gray-400 mb-2.5"></div>*/}
+          {/*  <div className="h-8 bg-gray-200 rounded-lg dark:bg-gray-400 mb-2.5"></div>*/}
+          {/*  <div className="h-8 bg-gray-200 rounded-lg dark:bg-gray-400"></div>*/}
+          {/*</div>*/}
+        </div>
+      ) : (
+        <div className="flex flex-col gap-6">
+          <UserSpecifications style={"flex-row items-center justify-between"} />
+          <hr />
+          <PersonalFile />
+          <UserTabsCard
+            data={panelTabData}
+            triangleState={triangleState}
+            setLogoutModalState={setLogoutModalState}
+            onclick={tabClickHandler}
+          />
+        </div>
+      )}
+      {logoutModalState && (
+        <LogoutModal setLogoutModalState={setLogoutModalState} />
+      )}
+    </div>
+  );
+};
+
+export default UserPanel;
