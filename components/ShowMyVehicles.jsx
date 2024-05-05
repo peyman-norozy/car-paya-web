1ecaf93786ec11e20ba22a97d198b2ded8867739
Iman Keramati <imankeramati1401@gmail.com>
2024-05-06 00:25:59 +0330

    handle setting vehicle slug in the url in desktop

--- components/ShowMyVehicles.jsx
+++ components/ShowMyVehicles.jsx
@@ -1,8 +1,14 @@
 import Image from "next/image";
 import React from "react";
+import {usePathname} from "next/navigation";
 
 const ShowMyVehicles = (props) => {
+    const pathname = usePathname()
+    console.log(pathname.split('/')[1])
   const selectItemHandler = (e) => {
+      const newUrl = '/' + pathname.split('/')[1] + '/' + e.currentTarget.getAttribute('title')
+      window.history.replaceState({ ...window.history.state, as: newUrl, url: newUrl }, '', newUrl)
+    console.log(e.currentTarget.getAttribute('title'))
     props.setSelectedItem(e.currentTarget.id)
     if(props.step === 'car-brands') {
         props.setStep('car-models')
@@ -15,11 +21,11 @@ const ShowMyVehicles = (props) => {
       props.setMotorStep('motor-tips')
   }
   }
-  
+
   return (
     <div className="grid grid-cols-4 gap-x-[0.5rem] gap-y-[1rem] rounded-10 border-gray_light_border border-[1px] w-full h-[12rem] p-[1rem] overflow-y-scroll">
       {props.data.map((item, index) => (
-        <div key={index} id={item.id} onClick={selectItemHandler} className="flex flex-col items-center gap-[0.25rem]  w-full cursor-pointer">
+        <div key={index} title={item.slug} id={item.id} onClick={selectItemHandler} className="flex flex-col items-center gap-[0.25rem]  w-full cursor-pointer">
           <div className="h-[60px] w-[70px] rounded-5 overflow-hidden">
             <Image
               src={props.step === 'car-brands' ? process.env.BASE_API + "/web/file/" + item.logo : process.env.BASE_API + "/web/file/" + item.image}
