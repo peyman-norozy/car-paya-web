50b86bf0e1adfbb63ba7b222afe666920fddc755
peyman norozy <peyman.norozy@gmail.com>
2024-05-10 12:50:23 +0330

    custom pwa modal in chrome and safari and firefox

---
+++ components/PwaModal.jsx
@@ -0,0 +1,8 @@
+'use client'
+import React from 'react'
+import AddToHomeScreen from "@/components/AddToHomeScreen/AddToHomScreen";
+const PwaModal = () => {
+    return (<AddToHomeScreen />)
+};
+
+export default PwaModal;
