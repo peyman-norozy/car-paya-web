e06b7a18105fa0651961a3f7f1c598b6105030e6
Iman Keramati <imankeramati1401@gmail.com>
2024-05-14 00:40:10 +0330

    getting second step data

--- components/SelectReserveTimeAndDate.jsx
+++ components/SelectReserveTimeAndDate.jsx
@@ -1,23 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import TimeLine from './TimeLine';
+import {persianDateCovertor, timestampToPersianDate, timeStampToTime} from "@/utils/function-utils";
 
 const SelectReserveTimeAndDate = (props) => {
-    const {day} = props
-    const timeData = [
-        {time : '12:00 - 10:00'},
-        {time : '12:00 - 10:00'},
-        {time : '12:00 - 10:00'},
-        {time : '12:00 - 10:00'},
-        {time : '12:00 - 10:00'},
-    ]
+    const {day  ,data, setIsSelected , isSelected} = props
+
+    console.log(data)
     return (
         <div className='bg-[#f6f6f6] rounded-[0.5rem] shadow-[0_0_15px_2px_rgba(0,0,0,0.15)] p-[1rem] flex items-center gap-[5rem] size690:gap-0 size690:justify-between'>
             <div className='flex flex-col items-center justify-center'>
-                <p className='font-bold text-16'>1403/01/01</p>
-                <p className='text-[13px]'>{day}</p>
+                <p className='font-bold text-16'>{persianDateCovertor(day)}</p>
+                <p className='text-[13px]'>day</p>
             </div>
             <div className='flex items-center gap-[1.5rem]'>
-                {timeData.map((item,index) => <TimeLine key={index} time={item.time} />)}
+                {data.map((item,index) => <TimeLine isSelected={item.id === isSelected} onclick={() => setIsSelected(item.id)} key={index} time={item} setIsSelected={setIsSelected} />)}
             </div>
         </div>
     );
--- components/TimeLine.jsx
+++ components/TimeLine.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
 const TimeLine = (props) => {
-    const {time} = props
+    const {time , isSelected , onclick} = props
     return (
-        <div className='cursor-pointer rounded-5 py-[0.4rem] px-[0.75rem] bg-white'>
-            {time}
+        <div className={`cursor-pointer rounded-5 py-[0.4rem] px-[0.75rem] bg-white ${isSelected && 'bg-green-700 text-white'}`} onClick={onclick}>
+
+            {time.start_time}  تا  {time.end_time}
         </div>
     );
 };
