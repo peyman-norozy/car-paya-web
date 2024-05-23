11f269d28d94fad954825fd585196e3c6fcc78a5
peyman norozy <peyman.norozy@gmail.com>
2024-05-23 13:29:53 +0330

    delete extra code from todoSlice.js

--- store/todoSlice.js
+++ store/todoSlice.js
@@ -2,81 +2,63 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    loginOtpData: {},
-    windowInnerWidth: 0,
-    loginState: true,
-    triangleState: false,
-    modalState: false,
-    recordModalCreateState: false,
-    selectVehicle:{},
-    carYear:[],
-    verificationLogin : false,
-    vehicleId:"",
-    tipId:0
+  loginOtpData: {},
+  windowInnerWidth: 0,
+  loginState: true,
+  triangleState: false,
+  selectVehicle: {},
+  carYear: [],
+  verificationLogin: false,
+  tipId: 0,
 };
 
 const todoSlice = createSlice({
-    name: "todos",
-    initialState,
-    reducers: {
-        getLoginOtpData(state, action) {
-            state.loginOtpData = action.payload;
-        },
-        getWindowInnerWidth(state, action) {
-            state.windowInnerWidth = action.payload;
-        },
-        setLoginState(state, action) {
-            state.loginState = action.payload;
-        },
-        setTriangleState(state, action) {
-            state.triangleState = action.payload;
-        },
-        setRecordModalState(state, action) {
-            state.modalState = action.payload;
-        },
-        setRecordModalCreateState(state, action) {
-            state.recordModalCreateState = action.payload;
-        },
-        setSelectCarBrand(state,action){
-            state.selectVehicle.carBrand = action.payload
-        },
-        setSelectCarModel(state,action){
-            state.selectVehicle.carModel = action.payload
-        },
-        setSelectCarTip(state,action){
-            state.selectVehicle.carTip = action.payload
-        },
-        setCarYear(state,action){
-            state.carYear = action.payload
-        },
-        setVehicleId(state,action){
-          state.vehicleId = action.payload
-        },
-        setTipId(state,action){
-            state.tipId = action.payload
-        },
-        setVerificationLogin(state, action) {
-            state.verificationLogin = action.payload;
-        },
+  name: "todos",
+  initialState,
+  reducers: {
+    getLoginOtpData(state, action) {
+      state.loginOtpData = action.payload;
     },
+    getWindowInnerWidth(state, action) {
+      state.windowInnerWidth = action.payload;
+    },
+    setLoginState(state, action) {
+      state.loginState = action.payload;
+    },
+    setTriangleState(state, action) {
+      state.triangleState = action.payload;
+    },
+    setSelectCarBrand(state, action) {
+      state.selectVehicle.carBrand = action.payload;
+    },
+    setSelectCarModel(state, action) {
+      state.selectVehicle.carModel = action.payload;
+    },
+    setSelectCarTip(state, action) {
+      state.selectVehicle.carTip = action.payload;
+    },
+    setCarYear(state, action) {
+      state.carYear = action.payload;
+    },
+    setTipId(state, action) {
+      state.tipId = action.payload;
+    },
+    setVerificationLogin(state, action) {
+      state.verificationLogin = action.payload;
+    },
+  },
 });
 
 export const {
-    getLoginOtpData,
-    getWindowInnerWidth,
-    setLoginState,
-    setTriangleState,
-    setRecordModalState,
-    setRecordModalCreateState,
-    setSelectCarBrand,
-    setSelectCarModel,
-    setSelectCarTip,
-    setCarYear,
-    setVehicleId,
-    setTipId,
-    setVerificationLogin
-} = todoSlice.actions
+  getLoginOtpData,
+  getWindowInnerWidth,
+  setLoginState,
+  setTriangleState,
+  setSelectCarBrand,
+  setSelectCarModel,
+  setSelectCarTip,
+  setCarYear,
+  setTipId,
+  setVerificationLogin,
+} = todoSlice.actions;
 export default todoSlice;
-
-
-
