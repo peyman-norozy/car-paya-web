397ffadfb9f3ddc27f144ff273ebc60f435f3c9b
peyman norozy <peyman.norozy@gmail.com>
2024-05-24 18:11:36 +0330

    chnnge the MainBanner data and add new icon for it

--- staticData/data.js
+++ staticData/data.js
@@ -1,193 +1,198 @@
 export const serviceData = [
-    {
-        title: "کارشناسی خودرو",
-        icon: "/assets/images/verification.png",
-        href: "/vehicle-verification",
-        id: "vehicleVerification"
-    },
-    {
-        title: "دیتیلینگ",
-        icon: "/assets/images/detailing.png",
-        href: "#",
-        id: "detailing"
-    },
-    {
-        title: "سرویس دوره ای",
-        icon: "/assets/images/periodicservice.png",
-        href: "/periodic-service",
-        id: "periodicService"
-    },
-    {
-        title: "فروشگاه باتری",
-        icon: "/assets/images/battery.png",
-        href: "/batteries",
-        id: "batteries"
-    },
-    {
-        title: "فروشگاه لوازم یدکی",
-        icon: "/assets/images/filter.png",
-        href: "/products",
-        id: "products"
-    },
-    {
-        title: "بیمه",
-        icon: "/assets/images/insurancecar.png",
-        href: "#",
-        id: "insurance"
-    },
-    {
-        title: "خرید خودرو",
-        icon: "/assets/images/buycar.png",
-        href: "#",
-        id: "#"
-    },
-    {
-        title: "فروش خودرو",
-        icon: "/assets/images/sellCar.png",
-        href: "#",
-        id: "#"
-    },
-    {
-        title: "شناسنامه و سوابق",
-        icon: "/assets/images/records.png",
-        href: "/panel/my-vehicle/my-car",
-        id: "myVehicleMyCar"
-    },
+  {
+    title: "کارشناسی خودرو",
+    icon: "/assets/images/CARSHENASI.webp",
+    href: "/vehicle-verification",
+    id: "vehicleVerification",
+  },
+  {
+    title: "دیتیلینگ",
+    icon: "/assets/images/DET.webp",
+    href: "#",
+    id: "detailing",
+  },
+  {
+    title: "سرویس دوره ای",
+    icon: "/assets/images/periodicservice.png",
+    href: "/periodic-service",
+    id: "periodicService",
+  },
+  {
+    title: "باتری",
+    icon: "/assets/images/BATRY.webp",
+    href: "/batteries",
+    id: "batteries",
+  },
+  {
+    title: "لوازم یدکی",
+    icon: "/assets/images/filter.png",
+    href: "/products",
+    id: "products",
+  },
+  {
+    title: "بیمه",
+    icon: "/assets/images/insurancecar.png",
+    href: "#",
+    id: "insurance",
+  },
+  {
+    title: "خرید خودرو",
+    icon: "/assets/images/buycar.png",
+    href: "#",
+    id: "#",
+  },
+  {
+    title: "فروش خودرو",
+    icon: "/assets/images/sellCar.png",
+    href: "#",
+    id: "#",
+  },
+  {
+    title: "شناسنامه و سوابق",
+    icon: "/assets/images/records.png",
+    href: "/panel/my-vehicle/my-car",
+    id: "myVehicleMyCar",
+  },
 ];
 export const HowWorksMockUpData = [
-    {
-        title: "انتخاب وسیله نقلیه",
-        description:
-            "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ است. ",
-        img: "/assets/icons/Perfect-Car-Service.png",
-    },
-    {
-        title: "انتخاب خدمات مورد نیاز",
-        description:
-            "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ است. ",
-        img: "/assets/icons/Doorstep-Pick-up.png",
-    },
-    {
-        title: "انتخاب مکان دریافت خدمات",
-        description:
-            "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ است. ",
-        img: "/assets/icons/service-real-time.png",
-    },
-    {
-        title: "انتخاب زمان دریافت خدمات",
-        description:
-            "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ است. ",
-        img: "/assets/icons/Doorstep-Pick-up.png",
-    },
-    {
-        title: "دریافت خدمات",
-        description:
-            "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ است. ",
-        img: "/assets/icons/Earn-While-We-Service.png",
-    },
+  {
+    title: "انتخاب وسیله نقلیه",
+    description:
+      "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ است. ",
+    img: "/assets/icons/Perfect-Car-Service.png",
+  },
+  {
+    title: "انتخاب خدمات مورد نیاز",
+    description:
+      "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ است. ",
+    img: "/assets/icons/Doorstep-Pick-up.png",
+  },
+  {
+    title: "انتخاب مکان دریافت خدمات",
+    description:
+      "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ است. ",
+    img: "/assets/icons/service-real-time.png",
+  },
+  {
+    title: "انتخاب زمان دریافت خدمات",
+    description:
+      "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ است. ",
+    img: "/assets/icons/Doorstep-Pick-up.png",
+  },
+  {
+    title: "دریافت خدمات",
+    description:
+      "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ است. ",
+    img: "/assets/icons/Earn-While-We-Service.png",
+  },
 ];
 export const CarProductsMockUpData = [
-    {
-        discount: "تخفیف ۲۰٪",
-        imgSrc: "/assets/images/hiclipart.com (2).png",
-        alt: "oil",
-        title: "روغن موتور بوش مدل لاکچری SN حجم ۴ لیتر",
-        newPrice: "",
-        oldPrice: "",
-    },
-    {
-        discount: "تخفیف ۲۰٪",
-        imgSrc: "/assets/images/hiclipart.com (3).png",
-        alt: "oil",
-        title: "روغن موتور بوش مدل لاکچری SN حجم ۴ لیتر",
-        newPrice: "",
-        oldPrice: "",
-    },
-    {
-        discount: "تخفیف ۲۰٪",
-        imgSrc: "/assets/images/hiclipart.com.png",
-        alt: "oil",
-        title: "فیلتر هوا جک 3s 65746",
-        newPrice: "",
-        oldPrice: "",
-    },
-    {
-        discount: "تخفیف ۲۰٪",
-        imgSrc: "/assets/images/hiclipart.com (1).png",
-        alt: "oil",
-        title: "فیلتر هوا جک 3s 65746",
-        newPrice: "",
-        oldPrice: "",
-    },
-    {
-        discount: "تخفیف ۲۰٪",
-        imgSrc: "/assets/images/hiclipart.com (1).png",
-        alt: "oil",
-        title: "فیلتر هوا جک 3s 65746",
-        newPrice: "",
-        oldPrice: "",
-    },
-    {
-        discount: "تخفیف ۲۰٪",
-        imgSrc: "/assets/images/hiclipart.com (1).png",
-        alt: "oil",
-        title: "فیلتر هوا جک 3s 65746",
-        newPrice: "",
-        oldPrice: "",
-    },
-    {
-        discount: "تخفیف ۲۰٪",
-        imgSrc: "/assets/images/hiclipart.com (1).png",
-        alt: "oil",
-        title: "فیلتر هوا جک 3s 65746",
-        newPrice: "",
-        oldPrice: "",
-    },
+  {
+    discount: "تخفیف ۲۰٪",
+    imgSrc: "/assets/images/hiclipart.com (2).png",
+    alt: "oil",
+    title: "روغن موتور بوش مدل لاکچری SN حجم ۴ لیتر",
+    newPrice: "",
+    oldPrice: "",
+  },
+  {
+    discount: "تخفیف ۲۰٪",
+    imgSrc: "/assets/images/hiclipart.com (3).png",
+    alt: "oil",
+    title: "روغن موتور بوش مدل لاکچری SN حجم ۴ لیتر",
+    newPrice: "",
+    oldPrice: "",
+  },
+  {
+    discount: "تخفیف ۲۰٪",
+    imgSrc: "/assets/images/hiclipart.com.png",
+    alt: "oil",
+    title: "فیلتر هوا جک 3s 65746",
+    newPrice: "",
+    oldPrice: "",
+  },
+  {
+    discount: "تخفیف ۲۰٪",
+    imgSrc: "/assets/images/hiclipart.com (1).png",
+    alt: "oil",
+    title: "فیلتر هوا جک 3s 65746",
+    newPrice: "",
+    oldPrice: "",
+  },
+  {
+    discount: "تخفیف ۲۰٪",
+    imgSrc: "/assets/images/hiclipart.com (1).png",
+    alt: "oil",
+    title: "فیلتر هوا جک 3s 65746",
+    newPrice: "",
+    oldPrice: "",
+  },
+  {
+    discount: "تخفیف ۲۰٪",
+    imgSrc: "/assets/images/hiclipart.com (1).png",
+    alt: "oil",
+    title: "فیلتر هوا جک 3s 65746",
+    newPrice: "",
+    oldPrice: "",
+  },
+  {
+    discount: "تخفیف ۲۰٪",
+    imgSrc: "/assets/images/hiclipart.com (1).png",
+    alt: "oil",
+    title: "فیلتر هوا جک 3s 65746",
+    newPrice: "",
+    oldPrice: "",
+  },
 ];
 export const periodicServiceUnderCardData = [
-    {
-        title: "سرویس دوره ای اقتصادی",
-        titleDescription: "(شامل ۱۷ نوع خدمات)",
-        options: [
-            "تعویض روغن موتور",
-            "تعویض فیلتر هوا",
-            "تعویض فیلتر روغن",
-            "تعویض فیلتر کابین",
-        ],
-    },
-    {
-        title: "سرویس دوره‌ای استاندارد",
-        titleDescription: "(شامل ۱۷ نوع خدمات)",
-        options: [
-            "تعویض روغن موتور",
-            "تعویض فیلتر هوا",
-            "تعویض فیلتر روغن",
-            "تعویض فیلتر کابین",
-        ],
-    },
-    {
-        title: "سرویس دوره‌ای ویژه",
-        titleDescription: "(شامل ۱۷ نوع خدمات)",
-        options: [
-            "تعویض روغن موتور",
-            "تعویض فیلتر هوا",
-            "تعویض فیلتر روغن",
-            "تعویض فیلتر کابین",
-        ],
-    },
+  {
+    title: "سرویس دوره ای اقتصادی",
+    titleDescription: "(شامل ۱۷ نوع خدمات)",
+    options: [
+      "تعویض روغن موتور",
+      "تعویض فیلتر هوا",
+      "تعویض فیلتر روغن",
+      "تعویض فیلتر کابین",
+    ],
+  },
+  {
+    title: "سرویس دوره‌ای استاندارد",
+    titleDescription: "(شامل ۱۷ نوع خدمات)",
+    options: [
+      "تعویض روغن موتور",
+      "تعویض فیلتر هوا",
+      "تعویض فیلتر روغن",
+      "تعویض فیلتر کابین",
+    ],
+  },
+  {
+    title: "سرویس دوره‌ای ویژه",
+    titleDescription: "(شامل ۱۷ نوع خدمات)",
+    options: [
+      "تعویض روغن موتور",
+      "تعویض فیلتر هوا",
+      "تعویض فیلتر روغن",
+      "تعویض فیلتر کابین",
+    ],
+  },
 ];
 export const workData = [
-    {
-        title: "تعویض روغن",
-        price: 3000000,
-        has_come: 1608663149,
-    },
-    {
-        title: "تعویض روغن",
-        price: 3000000,
-        has_come: 1708863149,
-    },
+  {
+    title: "تعویض روغن",
+    price: 3000000,
+    has_come: 1608663149,
+  },
+  {
+    title: "تعویض روغن",
+    price: 3000000,
+    has_come: 1708863149,
+  },
 ];
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default {serviceData, HowWorksMockUpData,periodicServiceUnderCardData,workData}
+export default {
+  serviceData,
+  HowWorksMockUpData,
+  periodicServiceUnderCardData,
+  workData,
+};
