92f8a405ae15e8314302cd8f3b362759590815e4
peyman norozy <peyman.norozy@gmail.com>
2024-05-26 01:27:24 +0330

    delete unusable code

--- components/cards/RecordModalCreatedCard.jsx
+++ components/cards/RecordModalCreatedCard.jsx
@@ -2,16 +2,10 @@ import React from "react";
 import CarBackgroundCard from "@/components/cards/CarBackgroundCard";
 import { persianDateCovertor } from "@/utils/function-utils";
 
-const arrayOfExample = [1, 2, 3, 4, 5, 6];
 const RecordModalCreatedCard = (props) => {
   const { item } = props;
   return (
     <CarBackgroundCard>
-      {/*{arrayOfExample.map((item, index) => (*/}
-      {/*  <div key={index} className="flex-1 text-center">*/}
-      {/*    sdf*/}
-      {/*  </div>*/}
-      {/*))}*/}
       <div className="flex-1 text-center">{props.index + 1}</div>
       <div className="flex-1 text-center">{persianDateCovertor(item.date)}</div>
       <div className="flex-1 text-center">
