83fdb19b40a9533401cea9bcba542af7a2912739
peyman norozy <peyman.norozy@gmail.com>
2024-06-09 00:16:35 +0330

    add the OrdersCurrentCard to the Refuse.jsx

--- components/Refuse.jsx
+++ components/Refuse.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import OrdersCurrentCard from "@/components/cards/OrdersCurrentCard";
+const fakeData = [null, null, null, null, null];
 
 const Refuse = () => {
-  return <div>Refuse</div>;
+  return (
+    <div>
+      <ul className={"flex flex-col gap-[16px]"}>
+        {fakeData.map((item, index) => (
+          <OrdersCurrentCard key={index} />
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Refuse;
