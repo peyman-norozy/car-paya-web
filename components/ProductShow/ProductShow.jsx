f4cc7c9c5d5bc967a44d9285532222d28cd9cf29
peyman-norozy <peyman.norozy@gmail.com>
2024-06-30 19:50:15 +0330

    make a ProductShow.jsx

---
+++ components/ProductShow/ProductShow.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ProductSlider from "@/components/ProductSlider/ProductSlider";
+
+const ProductShow = () => {
+  return (
+    <div>
+      <div>
+        <ProductSlider />
+      </div>
+    </div>
+  );
+};
+
+export default ProductShow;
