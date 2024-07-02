5608a980b3bb568c5d08c89027568be156b428ea
peyman-norozy <peyman.norozy@gmail.com>
2024-07-02 16:24:34 +0330

    add the ProductShowSlider.jsx and custom it

---
+++ components/ProductShowSlider/ProductShowSlider.jsx
@@ -0,0 +1,72 @@
+"use client";
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/navigation";
+import "swiper/css/thumbs";
+
+// import "./styles.css";
+
+const fakeData = [0, 0, 0];
+
+// import required modules
+import { FreeMode, Navigation, Thumbs } from "swiper/modules";
+import Image from "next/image";
+const ProductShowSlider = () => {
+  const [thumbsSwiper, setThumbsSwiper] = useState(null);
+
+  return (
+    <div>
+      <div>
+        <div className={"w-[344px] h-[344px]"}>
+          <Swiper
+            style={{
+              "--swiper-navigation-color": "#fff",
+              "--swiper-pagination-color": "#fff",
+            }}
+            spaceBetween={10}
+            navigation={true}
+            thumbs={{ swiper: thumbsSwiper }}
+            modules={[FreeMode, Navigation, Thumbs]}
+            className="mySwiper2"
+          >
+            {fakeData.map((item, index) => (
+              <SwiperSlide key={index}>
+                <img
+                  src="https://swiperjs.com/demos/images/nature-1.jpg"
+                  className={"rounded-[5px]"}
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+        <div className={"w-[344px] mt-[16px] batteries"}>
+          <Swiper
+            onSwiper={setThumbsSwiper}
+            spaceBetween={16}
+            slidesPerView={4}
+            freeMode={true}
+            watchSlidesProgress={true}
+            modules={[FreeMode, Navigation, Thumbs]}
+            className="mySwiper"
+          >
+            {fakeData.map((item, index) => (
+              <SwiperSlide key={index}>
+                <img
+                  src="https://swiperjs.com/demos/images/nature-1.jpg"
+                  className={"rounded-[5px]"}
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductShowSlider;
