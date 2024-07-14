b2d851818b12c6645ac8d36957b3a1b87f0ba01a
farshad <farshad.fsn@gmail.com>
2024-07-14 17:17:37 +0330

    make select product page responsive

--- app/periodic-service/product-selection/page.jsx
+++ app/periodic-service/product-selection/page.jsx
@@ -2,7 +2,7 @@ import Invoice from "@/components/periodic-service-components/Invoice";
 import SelectProduct from "@/components/serviceSelectionComponents/SelectProduct";
 const ProductSelection = () => {
     return ( 
-        <div className="flex gap-10 max-w-[1676px] w-full m-auto mb-10 mt-28">
+        <div className="flex gap-4 xl:gap-10 max-w-[1676px] w-full m-auto mb-10 mt-5 lg:mt-28 px-4">
             <Invoice/>
             <SelectProduct/>
         </div>
