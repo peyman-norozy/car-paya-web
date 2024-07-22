bf519abc52294edf583bbe0b6e4075c07bff2d17
peyman-norozy <peyman.norozy@gmail.com>
2024-07-22 10:20:47 +0330

    add the provinces and city input search and select

--- components/cards/SelectVehicleBox.jsx
+++ components/cards/SelectVehicleBox.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import SelectVehicleTab from "../SelectVehicleTab";
 import SearchInput from "../SearchInput";
 import car from "@/public/assets/images/hoeny-Hyundai-car.png";
@@ -11,6 +11,8 @@ import Spinner from "../Spinner";
 import { useDispatch } from "react-redux";
 import { setVehicleData } from "@/store/todoSlice";
 import { usePathname } from "next/navigation";
+import useClickOutside from "@/hook/useClickOutside";
+import useSetQuery from "@/hook/useSetQuery";
 
 const SelectVehicleBox = (props) => {
   const pathName = usePathname();
@@ -22,6 +24,10 @@ const SelectVehicleBox = (props) => {
   const [heavyCarModel, setHeavyCarModel] = useState([]);
   const [filterData, setFilterData] = useState([]);
   const [vehicleData, setVehicleData] = useState([]);
+  const [provienceCity, setProvienceCity] = useState([]);
+  const [city, setCity] = useState([]);
+  const [cityName, setCityName] = useState("");
+  const [isSelected, setIsSelected] = useState(false);
 
   // const [step, setStep] = useState("car-brands");
   const [motorStep, setMotorStep] = useState("motor-brands");
@@ -29,6 +35,10 @@ const SelectVehicleBox = (props) => {
   const [isLoading, setIsLoading] = useState(false);
   const [image, setImage] = useState("");
   const dispatch = useDispatch();
+  const cityRef = useRef();
+  const close = useCallback(() => setIsSelected(false), []);
+  useClickOutside(cityRef, close);
+  const query = useSetQuery();
 
   const myVehicleData = [];
 
@@ -146,6 +156,18 @@ const SelectVehicleBox = (props) => {
   }, [props.step, motorStep]);
 
   useEffect(() => {
+    axios
+      .get(process.env.BASE_API + "/web/geo/province-cities")
+      .then((res) => {
+        if (res.data.status === "success") {
+          setProvienceCity(res.data.data);
+          setCity(res.data.data);
+        }
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  useEffect(() => {
     setVehicleData(
       props.searchParams["attribute_value"] === "car"
         ? carBrands
@@ -172,9 +194,80 @@ const SelectVehicleBox = (props) => {
     setFilterData(vehicleData);
   }, [vehicleData]);
 
+  const cityChangeHandler = (e) => {
+    setCityName(e.target.value);
+    setCity(provienceCity.filter((i) => i.label.includes(e.target.value)));
+
+    if (e.target.value.length >= 1) {
+      setIsSelected(true);
+    } else {
+      setIsSelected(false);
+    }
+  };
+
+  const focusInputHandler = () => {
+    setIsSelected(true);
+  };
+
+  const selectCityHandler = (e, cityId) => {
+    setCityName(e.target.innerHTML);
+    query.setQuery("provience_city_id", 87);
+    setIsSelected(false);
+  };
+
   return (
     <div className="shadow-[0_0_6px_0_rgba(177,177,177,1)] rounded-10">
       <div className="w-[95%] m-auto py-[1rem]">
+        {props.cityProvincesTitle && (
+          <p className={"text-14 font-medium mb-5"}>
+            {props.cityProvincesTitle}
+          </p>
+        )}
+        <div
+          className={
+            "rounded-lg flex items-center border border-[#B0B0B0] p-[10px] text-[#3D3D3D] h-[2.5rem] mb-[1rem] relative"
+          }
+        >
+          <input
+            ref={cityRef}
+            value={cityName}
+            id={"city"}
+            type={"text"}
+            className={"w-full h-full outline-none text-[#3D3D3D]"}
+            autoComplete={"off"}
+            onChange={cityChangeHandler}
+            onFocus={focusInputHandler}
+          />
+          <i className={"cc-arrow-down"} />
+          <label
+            htmlFor={"city"}
+            className={
+              "text-[#454545] text-14 absolute top-[-30%] bg-white right-[5%] px-1"
+            }
+          >
+            استان/شهر <span className={"text-RED_400"}> * </span>
+          </label>
+          <ul
+            // ref={cityRef}
+            className={`${isSelected ? cityRef.current.scrollHeight + "px px-[12px] py-2" : "h-0"}  shadow-[0_0_12px_rgba(226,226,226,0.8)] bg-white absolute right-0 top-[40px] w-full overflow-scroll max-h-[10rem]`}
+          >
+            {city.length > 0 ? (
+              city.map((item, index) => (
+                <li
+                  className={"py-[6px] pr-[12px] hover:bg-BLUE_100 rounded-lg"}
+                  onClick={(event) => selectCityHandler(event, item.cityId)}
+                  key={index}
+                >
+                  {item.label}
+                </li>
+              ))
+            ) : (
+              <p className={"py-[6px] pr-[12px] text-12 rounded-lg"}>
+                شهر یا استان مورد نظر یافت نشد
+              </p>
+            )}
+          </ul>
+        </div>
         <h1 className="text-text_gray w-full text-center mb-[0.5rem]">
           {props.title}
         </h1>
