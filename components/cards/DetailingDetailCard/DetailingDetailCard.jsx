b565ff7bafc9c9cb6fd4caa29802b1cc8ba7f425
peyman-norozy <peyman.norozy@gmail.com>
2024-07-27 15:07:52 +0330

    make DetailingDetailCard.jsx and custom it

---
+++ components/cards/DetailingDetailCard/DetailingDetailCard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const DetailingDetailCard = () => {
+  return (
+    <li className={"flex items-center gap-[8px]"}>
+      <span
+        className={"inline-block w-[20px] h-[20px] bg-[#24D34B] rounded-full"}
+      ></span>
+      <span>لورم ایپسوم متن ساختگی</span>
+    </li>
+  );
+};
+
+export default DetailingDetailCard;
