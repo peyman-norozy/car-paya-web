3ebefed326fd72271b848accfa623197aa3af8db
farshad <farshad.fsn@gmail.com>
2024-07-28 16:33:09 +0330

    add invoice to CarSelectComponent

--- components/public/CarSelectComponent.jsx
+++ components/public/CarSelectComponent.jsx
@@ -4,7 +4,8 @@ import axios from "axios";
 import Image from "next/image";
 import { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
-
+import invoice from "@/public/assets/images/invoice.png";
+import { usePathname } from "next/navigation";
 const CarSelectComponent = () => {
   const [vehicleType, setVehicleType] = useState("car");
   const [level, setLevel] = useState(1);
@@ -18,6 +19,7 @@ const CarSelectComponent = () => {
   const showHeaderData = useSelector((state) => state.todo.showHeader);
   const optionRef = useRef(null);
   const inputRef = useRef(null);
+  const pathName = usePathname();
   useEffect(() => {
     getBrandData("car");
     const object = localStorage.getItem("selectedVehicle");
@@ -51,6 +53,7 @@ const CarSelectComponent = () => {
     } else {
       setSelectedCity("");
     }
+    console.log(pathName);
   }, []);
 
   function vehicleTypeFetch(model) {
@@ -90,7 +93,7 @@ const CarSelectComponent = () => {
                 id: item.id,
                 title: item.title,
                 image: item.image,
-              }),
+              })
             );
             setCarSelected(true);
           }
@@ -108,60 +111,230 @@ const CarSelectComponent = () => {
     localStorage.setItem("city", JSON.stringify(item));
   }
 
-    return ( 
-        <div className="absolute h-full pb-24 top-0 right-auto">
-            <div className={`bg-[#383838A3] h-[605px] rounded-2xl w-[400px] sticky ${showHeaderData ? "top-[123px]" : "top-[10px]"} right-auto z-[2] backdrop-blur-[16px] p-4 hidden lg:flex flex-col gap-4`}>
-            {carSelected?
-                <div className="flex flex-col gap-6">
-                        <Image src={process.env.BASE_API+"/web"+API_PATHS.FILE+"/"+selectedCar.image} width={200} height={150} className="w-[60%] aspect-auto m-auto"/>
-                        <div className="flex justify-between items-center">
-                            <span className="font-bold text-18 text-[#FEFEFE] border-r-[5px] border-[#c0c0c0] leading-6 pr-2">{selectedCar.title}</span>
-                            <button className="text-[#F66B34] text-16 cursor-pointer font-medium" onClick={()=>{setCarSelected(false) , localStorage.removeItem("selectedVehicle")}}>تغییر خودرو</button>
-                        </div>
-                        <div className="flex flex-col gap-3 items-start relative" onFocusCapture={()=>{setOptionState(true)}}>
-                            <span className="text-[#FEFEFE] font-bold">
-                                انتخاب استان / شهر
-                            </span>
-                            <input className="w-full bg-[#FEFEFE] rounded-lg text-[#0E0E0E] h-10 outline-none px-2" value={selectedCity} onChange={(e)=>{inputChangeHandler(e.target.value)}} ref={inputRef}/>
-                            {optionState&&<div className="absolute w-[calc(100%-32px)] overflow-y-scroll bg-[#FEFEFE] rounded-b-lg top-[76px]">
-                                <div className="max-h-[200px] flex flex-col" ref={optionRef}>
-                                    {searchCity.map((item)=>(
-                                        <span className="cursor-pointer hover:bg-slate-200 py-1 px-2" value={item.id} onClick={(e)=>{cityClickHandler(item)}}>{item.label}</span>
-                                    ))}
-                                </div>
-                            </div>}
-                        </div>
-                </div>
-                :
-                <>
-                    <span className="text-[#FEFEFE] text-20 font-bold text-center">ثبت وسیله نقلیه</span>
-                    <div className="rounded-lg bg-[#F66B3414] flex justify-between p-1"> 
-                        <button className={`${vehicleType==="car"?"bg-[#F66B34] text-[#FEFEFE]":"text-[#F66B34]"} rounded-[4px] w-[100px] h-10 flex justify-center items-center font-medium text-14`} onClick={()=>{vehicleTypeFetch("car")}}>خودرو</button>
-                        <div className="my-2 w-[1px] bg-[#F66B34]"></div>
-                        <button className={`${vehicleType==="motor"?"bg-[#F66B34] text-[#FEFEFE]":"text-[#F66B34]"} rounded-[4px] w-[100px] h-10 flex justify-center items-center  font-medium text-14`} onClick={()=>{vehicleTypeFetch("motor")}}>موتورسیکلت</button>
-                        <div className="my-2 w-[1px] bg-[#F66B34]"></div>
-                        <button className={`${vehicleType==="heavy-car"?"bg-[#F66B34] text-[#FEFEFE]":"text-[#F66B34]"} rounded-[4px] w-[100px] h-10 flex justify-center items-center text-[#F66B34] font-medium text-14`} onClick={()=>{vehicleTypeFetch("heavy-car")}}>وسیله سنگین</button>
-                    </div>
-                    <div className="flex flex-col gap-4">
-                        <span className="text-center font-bold text-[#FEFEFE]">انتخاب برند</span>
-                        <div className="flex gap-2 py-1 px-4 text-[#B0B0B0] bg-[#B0B0B01F] rounded-lg">
-                            <i className="cc-search text-xl"/>
-                            <input className="outline-none text-14 bg-[#ffffff01]" placeholder="جستجو..."/>
-                        </div>
-                        <div className="h-[363px] overflow-y-scroll mt-2">
-                            <div className="grid grid-cols-3 gap-x-8 gap-y-[42px]">
-                                {data.map((item,index)=>(
-                                    <div className="flex flex-col items-center gap-2" key={index} onClick={()=>{optionClickHandler(item.id , item)}}>
-                                        <Image src={process.env.BASE_API+"/web"+API_PATHS.FILE+"/"+(item.logo?item.logo:item.image)} width={64} height={48} className="w-16 h-12"/>
-                                        <span className="text-white font-bold line-clamp-1 text-center">{item.title}</span>
-                                    </div>
-                                ))}
-                            </div>
-                        </div>
-                    </div>
-                </>
-            }
+  return (
+    <div className="absolute h-full pb-24 top-0 right-auto">
+      <div
+        className={`bg-[#383838A3] h-[605px] rounded-2xl w-[400px] sticky ${showHeaderData ? "top-[123px]" : "top-[10px]"} right-auto z-[2] backdrop-blur-[16px] p-4 hidden lg:flex flex-col gap-4`}
+      >
+        {carSelected ? (
+          <div className="flex flex-col gap-4">
+            <Image
+              src={
+                process.env.BASE_API +
+                "/web" +
+                API_PATHS.FILE +
+                "/" +
+                selectedCar.image
+              }
+              width={200}
+              height={150}
+              className="w-[60%] aspect-auto m-auto"
+            />
+            <div className="flex justify-between items-center">
+              <span className="font-bold text-18 text-[#FEFEFE] border-r-[5px] border-[#c0c0c0] leading-6 pr-2">
+                {selectedCar.title}
+              </span>
+              <button
+                className="text-[#F66B34] text-16 cursor-pointer font-medium"
+                onClick={() => {
+                  setCarSelected(false),
+                    localStorage.removeItem("selectedVehicle");
+                }}
+              >
+                تغییر خودرو
+              </button>
             </div>
+            {pathName !== "/periodic-service/service-selection" ? (
+              <>
+                <div
+                  className="flex flex-col gap-3 items-start relative"
+                  onFocusCapture={() => {
+                    setOptionState(true);
+                  }}
+                >
+                  <span className="text-[#FEFEFE] font-bold">
+                    انتخاب استان / شهر
+                  </span>
+                  <input
+                    className="w-full bg-[#FEFEFE] rounded-lg text-[#0E0E0E] h-10 outline-none px-2"
+                    value={selectedCity}
+                    onChange={(e) => {
+                      inputChangeHandler(e.target.value);
+                    }}
+                    ref={inputRef}
+                  />
+                  {optionState && (
+                    <div className="absolute w-[calc(100%-32px)] overflow-y-scroll bg-[#FEFEFE] rounded-b-lg top-[76px]">
+                      <div
+                        className="max-h-[200px] flex flex-col"
+                        ref={optionRef}
+                      >
+                        {searchCity.map((item) => (
+                          <span
+                            className="cursor-pointer hover:bg-slate-200 py-1 px-2"
+                            value={item.id}
+                            onClick={(e) => {
+                              cityClickHandler(item);
+                            }}
+                          >
+                            {item.label}
+                          </span>
+                        ))}
+                      </div>
+                    </div>
+                  )}
+                </div>
+                <div className="flex flex-col mt-[-10px] items-center">
+                  <Image src={invoice} className="m-auto size-52 opacity-70" />
+                  <span className="text-white">
+                    در حال حاضر سرویسی انتخاب نکرده اید
+                  </span>
+                </div>
+              </>
+            ) : (
+              <div className="flex flex-col gap-4">
+                <div className="flex flex-col gap-3 h-[196px] overflow-y-scroll">
+                  <div className="flex flex-col px-3 py-2 bg-[#888888] rounded-lg">
+                    <div className="flex justify-between">
+                      <span className="font-bold text-[#FEFEFE]">فیلتر هوا کاسپین</span>
+                      <div className="bg-[#FEFEFE] rounded-full size-5 text-[#888888] font-bold pr-[5px]">X</div>
+                    </div>
+                    <div className="flex justify-start gap-2 items-center">
+                      <span className="text-[#B0B0B0] line-through text-12 ">4.000.000 تومان</span>
+                      <span className={"size-1 bg-[#B0B0B0] rounded-full "}></span>
+                      <span className="text-[#FEFEFE] text-14 font-bold">3.000.000 تومان</span>
+                    </div>
+                  </div>
+                  <div className="flex flex-col px-3 py-2 bg-[#888888] rounded-lg">
+                    <div className="flex justify-between">
+                      <span className="font-bold text-[#FEFEFE]">فیلتر هوا کاسپین</span>
+                      <div className="bg-[#FEFEFE] rounded-full size-5 text-[#888888] font-bold pr-[5px]">X</div>
+                    </div>
+                    <div className="flex justify-start gap-2 items-center">
+                      <span className="text-[#B0B0B0] line-through text-12 ">4.000.000 تومان</span>
+                      <span className={"size-1 bg-[#B0B0B0] rounded-full "}></span>
+                      <span className="text-[#FEFEFE] text-14 font-bold">3.000.000 تومان</span>
+                    </div>
+                  </div>
+                  <div className="flex flex-col px-3 py-2 bg-[#888888] rounded-lg">
+                    <div className="flex justify-between">
+                      <span className="font-bold text-[#FEFEFE]">فیلتر هوا کاسپین</span>
+                      <div className="bg-[#FEFEFE] rounded-full size-5 text-[#888888] font-bold pr-[5px]">X</div>
+                    </div>
+                    <div className="flex justify-start gap-2 items-center">
+                      <span className="text-[#B0B0B0] line-through text-12 ">4.000.000 تومان</span>
+                      <span className={"size-1 bg-[#B0B0B0] rounded-full "}></span>
+                      <span className="text-[#FEFEFE] text-14 font-bold">3.000.000 تومان</span>
+                    </div>
+                  </div>
+                  <div className="flex flex-col px-3 py-2 bg-[#888888] rounded-lg">
+                    <div className="flex justify-between">
+                      <span className="font-bold text-[#FEFEFE]">فیلتر هوا کاسپین</span>
+                      <div className="bg-[#FEFEFE] rounded-full size-5 text-[#888888] font-bold pr-[5px]">X</div>
+                    </div>
+                    <div className="flex justify-start gap-2 items-center">
+                      <span className="text-[#B0B0B0] line-through text-12 ">4.000.000 تومان</span>
+                      <span className={"size-1 bg-[#B0B0B0] rounded-full "}></span>
+                      <span className="text-[#FEFEFE] text-14 font-bold">3.000.000 تومان</span>
+                    </div>
+                  </div>
+                </div>
+                <hr />
+                <div className="flex flex-col gap-2">
+                  <div className="text-[#fefefe] flex justify-between">
+                    <span className="text-14 font-bold">دستمزد</span>
+                    <span className="font-bold text-14">200.000 تومان</span>
+                  </div>
+                  <div className="text-[#fefefe] flex justify-between">
+                    <span className="text-14 font-bold">ایاب ذهاب</span>
+                    <span className="font-bold text-14">450.000 تومان</span>
+                  </div>
+                </div>
+                <hr />
+                <button className="bg-[#F66B34] rounded-md flex justify-between py-2 px-4">
+                  <span className="text-white font-semibold ">تکمیل سفارش</span>
+                  <span className="text-white font-semibold ">6.000.000 تومان</span>
+                </button>
+              </div>
+            )}
+          </div>
+        ) : (
+          <>
+            <span className="text-[#FEFEFE] text-20 font-bold text-center">
+              ثبت وسیله نقلیه
+            </span>
+            <div className="rounded-lg bg-[#F66B3414] flex justify-between p-1">
+              <button
+                className={`${vehicleType === "car" ? "bg-[#F66B34] text-[#FEFEFE]" : "text-[#F66B34]"} rounded-[4px] w-[100px] h-10 flex justify-center items-center font-medium text-14`}
+                onClick={() => {
+                  vehicleTypeFetch("car");
+                }}
+              >
+                خودرو
+              </button>
+              <div className="my-2 w-[1px] bg-[#F66B34]"></div>
+              <button
+                className={`${vehicleType === "motor" ? "bg-[#F66B34] text-[#FEFEFE]" : "text-[#F66B34]"} rounded-[4px] w-[100px] h-10 flex justify-center items-center  font-medium text-14`}
+                onClick={() => {
+                  vehicleTypeFetch("motor");
+                }}
+              >
+                موتورسیکلت
+              </button>
+              <div className="my-2 w-[1px] bg-[#F66B34]"></div>
+              <button
+                className={`${vehicleType === "heavy-car" ? "bg-[#F66B34] text-[#FEFEFE]" : "text-[#F66B34]"} rounded-[4px] w-[100px] h-10 flex justify-center items-center text-[#F66B34] font-medium text-14`}
+                onClick={() => {
+                  vehicleTypeFetch("heavy-car");
+                }}
+              >
+                وسیله سنگین
+              </button>
+            </div>
+            <div className="flex flex-col gap-4">
+              <span className="text-center font-bold text-[#FEFEFE]">
+                انتخاب برند
+              </span>
+              <div className="flex gap-2 py-1 px-4 text-[#B0B0B0] bg-[#B0B0B01F] rounded-lg">
+                <i className="cc-search text-xl" />
+                <input
+                  className="outline-none text-14 bg-[#ffffff01]"
+                  placeholder="جستجو..."
+                />
+              </div>
+              <div className="h-[363px] overflow-y-scroll mt-2">
+                <div className="grid grid-cols-3 gap-x-8 gap-y-[42px]">
+                  {data.map((item, index) => (
+                    <div
+                      className="flex flex-col items-center gap-2"
+                      key={index}
+                      onClick={() => {
+                        optionClickHandler(item.id, item);
+                      }}
+                    >
+                      <Image
+                        src={
+                          process.env.BASE_API +
+                          "/web" +
+                          API_PATHS.FILE +
+                          "/" +
+                          (item.logo ? item.logo : item.image)
+                        }
+                        width={64}
+                        height={48}
+                        className="w-16 h-12"
+                      />
+                      <span className="text-white font-bold line-clamp-1 text-center">
+                        {item.title}
+                      </span>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </>
+        )}
+      </div>
     </div>
   );
 };
