19588874345de362ecf92dc0b9203012c0d729ba
peyman-norozy <peyman.norozy@gmail.com>
2024-07-28 09:59:18 +0330

    delete unusable code

--- app/detailing/page.jsx
+++ app/detailing/page.jsx
@@ -1,27 +1,16 @@
 import React from "react";
-import CarSelectComponent from "@/components/public/CarSelectComponent";
-import CarServicesSlider from "@/components/CarServicesSlider/CarServicesSlider";
-import { serviceData } from "@/staticData/data";
 import DetailingCard from "@/components/cards/DetailingCard/DetailingCard";
 
 let fakeArray = [0, 0, 0, 0, 0, 0, 0];
 
 const Page = () => {
   return (
-    <div
-      className={
-        "flex lg:flex-row flex-col relative max-w-[1772px] mx-auto py-4 size666:px-12 lg:mt-[110px] mt-[20px]"
-      }
-    >
+    <div className={"flex flex-col relative py-4 max-w-[1772px] m-auto"}>
       <h1 className={"lg:hidden block text-center text-[20px] font-semibold"}>
         دیتیلینگ
       </h1>
-      <CarSelectComponent />
       <section className={"lg:w-[calc(100%-424px)] w-full mr-auto"}>
-        <div className={"lg:block hidden"}>
-          <CarServicesSlider data={serviceData} />
-        </div>
-        <ul className={"lg:mt-[40px] mt-[20px] flex flex-col gap-[24px]"}>
+        <ul className={"lg:mt-16 mt-[20px] flex flex-col gap-[24px]"}>
           {fakeArray.map((item, index) => (
             <DetailingCard key={index} />
           ))}
