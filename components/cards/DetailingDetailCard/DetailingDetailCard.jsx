73bc199f83dd7a7a11534bec6c49b9cbf31e52d9
peyman-norozy <peyman.norozy@gmail.com>
2024-07-28 10:00:14 +0330

    add the opacity to the background

--- components/cards/DetailingDetailCard/DetailingDetailCard.jsx
+++ components/cards/DetailingDetailCard/DetailingDetailCard.jsx
@@ -4,7 +4,7 @@ const DetailingDetailCard = () => {
   return (
     <li className={"flex items-center gap-[8px]"}>
       <span
-        className={"inline-block w-[20px] h-[20px] bg-[#24D34B] rounded-full"}
+        className={"inline-block w-[20px] h-[20px] bg-[#24D34B80] rounded-full"}
       ></span>
       <span>لورم ایپسوم متن ساختگی</span>
     </li>
