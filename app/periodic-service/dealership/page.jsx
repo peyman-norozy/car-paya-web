2d763e583f648b28d5062658838a5870179d6a1d
farshad <farshad.fsn@gmail.com>
2024-07-30 17:30:18 +0330

    replace DealershipCard with NewAddressCard

--- app/periodic-service/dealership/page.jsx
+++ app/periodic-service/dealership/page.jsx
@@ -1,12 +1,15 @@
-import DealershipCard from "@/components/Dealership/DealershipCard";
+import NewAddressCard from "@/components/cards/NewAddressCard/NewAddressCard";
 import { getData } from "@/utils/api-function-utils";
 
 const Dealership = async (props) => {
     const data = await getData(`/web/service-periodical?step=step-1&type=${props.searchParams.type}&city_id=${props.searchParams.city_id}&vehicle_tip_id=${props.searchParams.vehicle_tip_id}`);
+
     return ( 
         <div className="lg:flex items-start gap-8 mt-1 lg:mt-[108px] max-w-[1772px] m-auto py-4 relative">
           <div className={"w-full lg:w-[calc(100%-424px)] md:gap-10 mr-auto flex flex-col gap-6"}>
-            {data.data.map((item) =>(<DealershipCard data={item} params={props}/>))}
+            {data.data.map((item,index) =>(
+              <NewAddressCard key={index} status={"fixed"} item={item} nextUrl={"/periodic-service/service-selection"}/>
+              ))}
           </div>
       </div>
      );
