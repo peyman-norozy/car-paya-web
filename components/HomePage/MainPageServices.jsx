a70b22a60292cb6507512268e3fd4592203da372
farshad <farshad.fsn@gmail.com>
2024-07-31 16:37:36 +0330

    handle data in hompage services

--- components/HomePage/MainPageServices.jsx
+++ components/HomePage/MainPageServices.jsx
@@ -2,21 +2,24 @@
 import { useRef, useState } from "react";
 import carachar from "@/public/assets/images/carachar.png"
 import Image from "next/image";
+import { serviceData } from "@/staticData/data";
+import Link from "next/link";
 
 const MainPageServices = (props) => {
     const serviceRef = useRef()
-    const array10 = [1,2,3,4,5,6,7,8,9,10]
     const [openService,setOpenService] = useState(false)
 
     return ( 
         <>
         <div className={`transition-all duration-700 overflow-y-hidden mx-4 md:mx-0 h-[270px] sm:h-[260px]`} style={{height:`${openService?`${serviceRef.current.offsetHeight}px`:""}`}}>
             <div className="grid grid-cols-3 xl:grid-cols-4 gap-3 sm:gap-6" ref={serviceRef}>
-                {array10.map((item,index)=>(
-                    <div className="w-full p-3 sm:p-4 flex flex-col bg-[#E7E7E7] items-center gap-2 sm:gap-3 rounded-lg" key={index} >
-                        <Image className="size-8  sm:size-12" src={carachar} height={48} width={48} alt="carachar"/>
-                        <span className="text-12 sm:text-base text-[#0E0E0E]">کارشناسی</span>
-                    </div>
+                {serviceData.map((item,index)=>(
+                    <Link href={item.href}>
+                        <div className="w-full p-3 sm:p-4 flex flex-col bg-[#E7E7E7] items-center gap-2 sm:gap-3 rounded-lg" key={index} >
+                            <Image className="size-8  sm:size-12" src={carachar} height={48} width={48} alt="carachar"/>
+                            <span className="text-12 sm:text-base text-[#0E0E0E]">{item.title}</span>
+                        </div>
+                    </Link>
                 ))}
             </div>
         </div>
