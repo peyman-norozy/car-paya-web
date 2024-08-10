9f63badaf1032decad790df4fc32d86142eb8cff
peyman-norozy <peyman.norozy@gmail.com>
2024-08-10 14:07:01 +0330

    make ServicesPage.jsx and custom it

---
+++ components/ServicesPage.jsx
@@ -0,0 +1,25 @@
+"use client";
+
+import React from "react";
+import ServicesCard from "@/components/cards/ServicesCard/ServicesCard";
+let fakeArray = [0, 0, 0, 0, 0, 0, 0];
+
+const ServicesPage = (props) => {
+  console.log(props);
+  return (
+    <div className={"flex flex-col relative py-4 max-w-[1772px] m-auto"}>
+      <h1 className={"lg:hidden block text-center text-[20px] font-semibold"}>
+        دیتیلینگ
+      </h1>
+      <section className={"lg:w-[calc(100%-424px)] w-full mr-auto"}>
+        <ul className={"lg:mt-16 mt-[20px] flex flex-col gap-[24px]"}>
+          {fakeArray.map((item, index) => (
+            <ServicesCard key={index} />
+          ))}
+        </ul>
+      </section>
+    </div>
+  );
+};
+
+export default ServicesPage;
