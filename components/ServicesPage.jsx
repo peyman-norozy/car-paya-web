be1d1f7cbaa2f65f63a271462d6fcfa7a7edf913
peyman-norozy <peyman.norozy@gmail.com>
2024-08-10 14:57:47 +0330

    get the data from the data

--- components/ServicesPage.jsx
+++ components/ServicesPage.jsx
@@ -2,10 +2,10 @@
 
 import React from "react";
 import ServicesCard from "@/components/cards/ServicesCard/ServicesCard";
-let fakeArray = [0, 0, 0, 0, 0, 0, 0];
 
 const ServicesPage = (props) => {
-  console.log(props);
+  const { data } = props;
+  console.log(data);
   return (
     <div className={"flex flex-col relative py-4 max-w-[1772px] m-auto"}>
       <h1 className={"lg:hidden block text-center text-[20px] font-semibold"}>
@@ -13,8 +13,8 @@ const ServicesPage = (props) => {
       </h1>
       <section className={"lg:w-[calc(100%-424px)] w-full mr-auto"}>
         <ul className={"lg:mt-16 mt-[20px] flex flex-col gap-[24px]"}>
-          {fakeArray.map((item, index) => (
-            <ServicesCard key={index} />
+          {data.map((item, index) => (
+            <ServicesCard key={item.id} {...item} />
           ))}
         </ul>
       </section>
