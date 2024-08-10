996d079624e80a972d215b6e0281bd1ff5361754
peyman-norozy <peyman.norozy@gmail.com>
2024-08-10 14:51:50 +0330

    add the condition to the DetailingDetailCard.jsx

--- components/cards/DetailingDetailCard/DetailingDetailCard.jsx
+++ components/cards/DetailingDetailCard/DetailingDetailCard.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 
 const DetailingDetailCard = (props) => {
+  const { key, label, value } = props;
   return (
     <li className={"flex items-center gap-[8px] w-[200px] text-14"}>
-      <span
-        className={"inline-block w-[20px] h-[20px] bg-[#24D34B80] rounded-full"}
-      ></span>
-      <span className={"line-clamp-1"}>{props.item}</span>
+      {value ? (
+        <span
+          className={
+            "inline-block w-[20px] h-[20px] bg-[#24D34B80] rounded-full"
+          }
+        ></span>
+      ) : (
+        <span
+          className={"inline-block w-[20px] h-[20px] bg-red-400 rounded-full"}
+        ></span>
+      )}
+      <span className={"line-clamp-1"}>{label}</span>
     </li>
   );
 };
