56cfa060e383f3fb6007390c1d17e97eb2f67087
peyman-norozy <peyman.norozy@gmail.com>
2024-08-10 09:21:34 +0330

    change code to the Link

--- app/detailing/page.jsx
+++ app/detailing/page.jsx
@@ -1,22 +1,14 @@
 import React from "react";
-import ServicesCard from "@/components/cards/ServicesCard/ServicesCard";
-
-let fakeArray = [0, 0, 0, 0, 0, 0, 0];
+import Link from "next/link";
 
 const Page = () => {
   return (
-    <div className={"flex flex-col relative py-4 max-w-[1772px] m-auto"}>
-      <h1 className={"lg:hidden block text-center text-[20px] font-semibold"}>
-        دیتیلینگ
-      </h1>
-      <section className={"lg:w-[calc(100%-424px)] w-full mr-auto"}>
-        <ul className={"lg:mt-16 mt-[20px] flex flex-col gap-[24px]"}>
-          {fakeArray.map((item, index) => (
-            <ServicesCard key={index} />
-          ))}
-        </ul>
-      </section>
-    </div>
+    <Link
+      href={"/detailing/services"}
+      className={"inline-block mt-[200px] mr-[400px] bg-red-600"}
+    >
+      دیتیلینگ
+    </Link>
   );
 };
 
