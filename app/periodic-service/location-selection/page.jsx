b0710312d8ce46a2b0cbaa8b414a1df9544f95d2
peyman-norozy <peyman.norozy@gmail.com>
2024-08-11 09:26:54 +0330

    add the params object to the fetch

--- app/periodic-service/location-selection/page.jsx
+++ app/periodic-service/location-selection/page.jsx
@@ -7,30 +7,41 @@ import { useCallback, useEffect, useState } from "react";
 const Dealership = (props) => {
   const [myLocationData, setMyLocationData] = useState([]);
   const [carCheckLocations, setCarCheckLocations] = useState([]);
-  const [filter , setFilter] = useState([])
-  const searchParams = useSearchParams()
-  const timeData = useCallback((query) => {
-    (async () => {
-      const fetchTimeData = await getDataWithFullErrorRes(
-        `/web/service-periodical?step=step-1&${searchParams.toString()}${query?query:""}`,
-      );
-      
-      if (searchParams.get("type") === "FIXED") {
-        setFilter(fetchTimeData.filter)
-        setCarCheckLocations(fetchTimeData.data);
-      } else if (searchParams.get("type") === "MOVING") {
-        setMyLocationData(fetchTimeData.data);
-      }
-    })();
-  }, [
-    searchParams.get("type"),
-    searchParams.get("city_id"),
-    searchParams.get("time_id")
-  ]);
+  const [filter, setFilter] = useState([]);
+  const searchParams = useSearchParams();
+  const timeData = useCallback(
+    (query) => {
+      (async () => {
+        const fetchTimeData = await getDataWithFullErrorRes(
+          `/web/service-periodical?step=step-1`,
+          {
+            city_id: searchParams.get("city_id"),
+            type: searchParams.get("type"),
+            vehicle_tip_id: searchParams.get("selectTipState").split(",")[1],
+          },
+        );
+
+        if (searchParams.get("type") === "FIXED") {
+          setFilter(fetchTimeData.filter);
+          console.log(fetchTimeData);
+          setCarCheckLocations(fetchTimeData.data);
+        } else if (searchParams.get("type") === "MOVING") {
+          setMyLocationData(fetchTimeData.data);
+        }
+      })();
+    },
+    [
+      searchParams.get("type"),
+      searchParams.get("city_id"),
+      searchParams.get("time_id"),
+    ],
+  );
 
   useEffect(() => {
     timeData();
   }, [searchParams.get("type")]);
+
+  console.log(carCheckLocations);
   return (
     <div className="lg:flex items-start gap-8 mt-1 lg:mt-[108px] max-w-[1772px] m-auto py-4 relative">
       <div
