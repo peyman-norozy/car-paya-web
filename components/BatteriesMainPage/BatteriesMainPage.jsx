63e3f4d1fdb6efffa5658be221ef6b31637a0da5
peyman-norozy <peyman.norozy@gmail.com>
2024-08-14 13:33:33 +0330

    import the custom component

--- components/BatteriesMainPage/BatteriesMainPage.jsx
+++ components/BatteriesMainPage/BatteriesMainPage.jsx
@@ -3,7 +3,12 @@
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { ToastContainer } from "react-toastify";
-import { useSearchParams } from "next/navigation";
+import Image from "next/image";
+import { batteryPurchaseProcessData } from "@/staticData/data";
+import BatteryPurchaseProcess from "@/components/cards/BatteryPurchaseProcess";
+import BatteryFaq from "@/components/BatteryFaq/BatteryFaq";
+// import BatteryAdvice from "@/components/BatteryAdvice/BatteryAdvice";
+import BatteryAdvice from "@/components/‌BatteryAdvice/‌BatteryAdvice";
 
 const BatteriesMainPage = () => {
   const [client, setClient] = useState(false);
@@ -22,12 +27,47 @@ const BatteriesMainPage = () => {
 
   return (
     <>
-      <Link
-        href={`batteries/products?attribute_slug=type_vehicle&attribute_value=car${TipId}`}
-        className={"bg-red-600 text-white p-2"}
+      <div className="bg-[#383838A3] rounded-3xl  flex size900:flex-row-reverse flex-col-reverse gap-6 p-6 items-center">
+        <div className="flex flex-col gap-2 items-start flex-1">
+          <h1 className="text-xl font-bold text-[#F66B34]">باتری</h1>
+          <p className="text-base text-[#FEFEFE] font-bold leading-8">
+            لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
+            استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در
+            ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و
+            کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی
+            در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را
+            می طلبد تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی
+            الخصوص طراحان خلاقی و فرهنگ پیشرو در زبان فارسی ایجاد کرد. در این
+            صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها و
+            شرایط سخت تایپ به پایان رسد وزمان مورد نیاز شامل حروفچینی دستاوردهای
+            اصلی و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد
+            استفاده قرار گیرد.
+          </p>
+          <Link
+            href={`batteries/products?attribute_slug=type_vehicle&attribute_value=car${TipId}`}
+            className="bg-[#F66B34] rounded-md py-2 px-4 text-[#FEFEFE] w-fit text-14 mt-2 font-medium"
+          >
+            ثبت درخواست
+          </Link>
+        </div>
+        <Image
+          alt={"buttery description image"}
+          src={"/assets/images/batteryIndex.png"}
+          width={245}
+          height={195}
+        />
+      </div>
+      <ul
+        className={
+          "flex justify-between gap-8 mt-10 overflow-x-scroll pb-2 [&>*:last-child]:hidden"
+        }
       >
-        batteries product
-      </Link>
+        {batteryPurchaseProcessData.map((item, index) => (
+          <BatteryPurchaseProcess key={index} {...item} />
+        ))}
+      </ul>
+      <BatteryAdvice />
+      <BatteryFaq />
       <ToastContainer rtl={true} />
     </>
   );
