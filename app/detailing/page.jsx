63411f53c3da8fe4520a4a48983fa88d7c82056c
peyman-norozy <peyman.norozy@gmail.com>
2024-08-14 17:48:53 +0330

    delete the servics data

--- app/detailing/page.jsx
+++ app/detailing/page.jsx
@@ -15,12 +15,26 @@ import React from "react";
 // export default Page;
 
 import PeriodicServiceIndex from "@/components/PeriodicServiceIndex/PeriodicServiceIndex";
+const servics = [
+  "پولیش",
+  "صفر شویی",
+  "کاور",
+  "سرامیک ۳ ساله",
+  "سرامیک ۵ ساله",
+  "سرامیک ۷ ساله",
+];
 
 const Page = () => {
   return (
     <div className="lg:flex items-start gap-8 mt-1 lg:mt-10 max-w-[1772px] m-auto  py-4 relative">
       <div className={"w-full lg:w-[calc(100%-424px)] mr-auto"}>
-        <PeriodicServiceIndex />
+        <PeriodicServiceIndex
+          title={"دیتیلینگ"}
+          servics={servics}
+          ImageAddress1={"/assets/images/detailingIndex1.jpg"}
+          ImageAddress2={"/assets/images/auto-detailing01.jpg"}
+          icon={"/assets/icons/detailingIconservices.png"}
+        />
       </div>
     </div>
   );
