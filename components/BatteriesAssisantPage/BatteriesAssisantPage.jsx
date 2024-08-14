979c36b9def8efff612eff9892e8da3dc58b23a0
peyman-norozy <peyman.norozy@gmail.com>
2024-08-14 13:30:42 +0330

    handle data in BatteriesAssisantPage.jsx

--- components/BatteriesAssisantPage/BatteriesAssisantPage.jsx
+++ components/BatteriesAssisantPage/BatteriesAssisantPage.jsx
@@ -1,9 +1,43 @@
 "use client";
-import React from "react";
-import { numberWithCommas } from "@/utils/function-utils";
+import React, { useState } from "react";
+import { error, numberWithCommas } from "@/utils/function-utils";
 import Button from "@/components/Button";
+import { setBatteriesData } from "@/store/todoSlice";
+import { useDispatch } from "react-redux";
+import PurchaseBatteryModal from "@/components/PurchaseBatteryModal";
+import { ToastContainer } from "react-toastify";
 
 const BatteriesAssisantPage = (props) => {
+  const [batteryIsSelected, setBatteryIsSelected] = useState(false);
+  const [filterButtery, setFilterButtery] = useState({});
+
+  const dispatch = useDispatch();
+  console.log(props);
+  const basketClickHandler = () => {
+    const CityId = JSON.parse(localStorage.getItem("city"))?.cityId;
+    if (Object.keys(filterButtery).length > 0) {
+      if (CityId) {
+        setBatteryIsSelected(true);
+        dispatch(setBatteriesData(filterButtery));
+      } else {
+        error("فیلد شهر و استان را انتخاب نشده");
+        alert("فیلد شهر و استان را انتخاب نشده");
+      }
+    } else {
+      alert("باتری خود را انتخاب کنید");
+      error("باتری خود را انتخاب کنید");
+    }
+  };
+
+  const radioButtonChangeHandler = (id) => {
+    console.log(id);
+    const singleButtery = props.data.data.filter((item) => item.id === id);
+    console.log(singleButtery);
+    setFilterButtery(...singleButtery);
+  };
+
+  console.log(filterButtery);
+
   return (
     <div className="flex flex-col relative py-4 max-w-[1772px] m-auto">
       <section className="lg:w-[calc(100%-424px)] w-full mr-auto lg:mt-16 mt-[20px]">
@@ -53,16 +87,17 @@ const BatteriesAssisantPage = (props) => {
                   <div className="checkbox-wrapper-37 flex justify-center items-center">
                     <input
                       type="radio"
-                      name={item.id}
-                      id="terms-checkbox-37"
+                      name={"terms-checkbox-37"}
+                      id={item.id}
                       className="hidden"
+                      onChange={() => radioButtonChangeHandler(item.id)}
                     />
                     <label
-                      htmlFor="terms-checkbox-37"
+                      htmlFor={item.id}
                       className="terms-label flex items-center cursor-pointer"
                     >
                       <svg
-                        className="checkbox-svg w-7 h-7 border-2 border-[#F66B34] rounded fill-none transition-all"
+                        className="checkbox-svg w-7 h-7 border-2 border-[#F66B34] rounded fill-none transition-all duration-300"
                         viewBox="0 0 200 200"
                         xmlns="http://www.w3.org/2000/svg"
                       >
@@ -85,13 +120,28 @@ const BatteriesAssisantPage = (props) => {
         </table>
       </section>
       <Button
-        // on_click={basketClickHandler}
+        on_click={basketClickHandler}
         class_name={
           "bg-[#F66B34] text-white h-[40px] lg:text-[16px] text-12 self-end rounded-[8px] size1400:w-[160px] w-[120px] mt-4"
         }
       >
         اضافه به سبد خرید
       </Button>
+
+      <div
+        className={`${!batteryIsSelected ? "hidden" : "fixed"} inset-0 h-full w-full bg-[#4c4c4caa] z-[20000] transition-all`}
+        onClick={() => setBatteryIsSelected(false)}
+      ></div>
+
+      <div
+        className={`w-[75%] size900:w-[50%] m-auto fixed transition-all duration-1000 ${batteryIsSelected ? "top-[50%]" : "top-[-60%]"} left-[50%] translate-x-[-50%] translate-y-[-50%] z-[20000]`}
+      >
+        <PurchaseBatteryModal
+          setBatteryIsSelected={setBatteryIsSelected}
+          batteryIsSelected={batteryIsSelected}
+        />
+      </div>
+      <ToastContainer rtl={true} />
     </div>
   );
 };
