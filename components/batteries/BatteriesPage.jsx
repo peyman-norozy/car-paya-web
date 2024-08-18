abdbdc7c313f3402971da55c50d2c1d8bf356b6c
peyman-norozy <peyman.norozy@gmail.com>
2024-08-18 08:38:31 +0330

    delete unusable code

--- components/batteries/BatteriesPage.jsx
+++ components/batteries/BatteriesPage.jsx
@@ -2,34 +2,23 @@
 
 import React, { useEffect, useRef, useState } from "react";
 import PurchaseBatteryModal from "@/components/PurchaseBatteryModal";
-
 import useSetQuery from "@/hook/useSetQuery";
-import { getDataWithFullErrorRes } from "@/utils/api-function-utils";
 import SubFilterCard from "@/components/cards/SubFilterCard";
 import { ToastContainer } from "react-toastify";
 import BatteriesCard from "@/components/cards/BatteriesCard/BatteriesCard";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useDispatch } from "react-redux";
-import { setCarSelectToastHandler, setCityModalState } from "@/store/todoSlice";
+import { setCityModalState } from "@/store/todoSlice";
 import Link from "next/link";
 import nProgress from "nprogress";
 
 const BatteriesPage = (props) => {
   const query = useSetQuery();
-  console.log(props.data);
-  console.log(props);
-  const [isClicked, setIsClicked] = useState(1);
   const [filterISOpen, setFilterIsOpen] = useState(false);
   const [batteryIsSelected, setBatteryIsSelected] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
   const [filter, setFilter] = useState("فیلتر بر اساس");
-  const [selectedItem, setSelectedItem] = useState(null);
-  const [step, setStep] = useState("car-brands");
-  const [modalStep, setModalStep] = useState(1);
-  const [selectFilterData, setSelectFilterData] = useState([]);
   const [topDistance, setTopDistance] = useState(0);
   const [filterModalState, setFilterModalState] = useState(false);
-  const [timeModalState, setTimeModalState] = useState(false);
   const [filterId, setFilterId] = useState("");
   const dispatch = useDispatch();
   const filterRef = useRef(null);
@@ -38,46 +27,10 @@ const BatteriesPage = (props) => {
   const heightRef = useRef(null);
   const searchParams = useSearchParams();
 
-  console.log(props);
-  // if (props.filterData === 500) {
-  //   console.log(props.filterData);
-  // }
-  console.log(props);
-  const data = selectFilterData.length > 0 ? selectFilterData : props.data.data;
-  const tabTitle = [
-    { name: "خودرو", attributeSlug: "type_vehicle", slug: "car" },
-    { name: "موتور سیکلت", attributeSlug: "type_vehicle", slug: "motor" },
-    { name: "وسیله سنگین", attributeSlug: "type_vehicle", slug: "heavy_car" },
-    { name: "وسیله من", attributeSlug: "type_vehicle", slug: "my_vehicle" },
-  ];
-
   const filterData = [
     { name: "آمپر", value: "getAmp" },
     { name: "برند", value: "brand" },
   ];
-
-  const brandOption = [
-    {
-      id: "amper",
-      amper: [
-        { value: "25", label: "باتری 25 آمپر" },
-        { value: "45", label: "باتری 45 آمپر" },
-        { value: "56", label: "باتری 56 آمپر" },
-        { value: "77", label: "باتری 77 آمپر" },
-      ],
-    },
-    {
-      id: "brand",
-      brand: [
-        { value: "سپاهان", label: "sepahan" },
-        { value: "صباباتری", label: "sababatry" },
-        { value: "آذر باتری", label: "azarbatry" },
-        { value: "کاسپین", label: "caspian" },
-      ],
-    },
-  ];
-
-  console.log(props);
 
   const toggleFilterHandler = () => {
     setFilterIsOpen((prevState) => !prevState);
@@ -133,7 +86,6 @@ const BatteriesPage = (props) => {
   }, []);
 
   useEffect(() => {
-    // console.log(JSON.parse(localStorage.getItem("selectedVehicle"))?.id);
     if (
       !JSON.parse(localStorage.getItem("city")) &&
       !JSON.parse(localStorage.getItem("city"))?.cityId
@@ -142,13 +94,6 @@ const BatteriesPage = (props) => {
       nProgress.start();
       router.push("/batteries");
     }
-    // if (
-    //   !JSON.parse(localStorage.getItem("selectedVehicle")) &&
-    //   !JSON.parse(localStorage.getItem("selectedVehicle"))?.id
-    // ) {
-    //   dispatch(setCarSelectToastHandler(true));
-    //   router.push("/batteries");
-    // }
   }, [dispatch, router, searchParams]);
 
   return (
@@ -163,7 +108,6 @@ const BatteriesPage = (props) => {
             className="bg-[#5D697A] rounded-[8px] py-[0.5rem] px-[1.5rem] flex items-center gap-[0.5rem] text-white cursor-pointer relative font-semibold"
           >
             <i className={"cc-arrow-down text-[20px]"} />
-            {/*<Image src={arrow} alt={"arrow"} cl width={10} height={10} />*/}
             <p className="text-12">{filter}</p>
             <ul
               ref={heightRef}
@@ -234,129 +178,10 @@ const BatteriesPage = (props) => {
           ))}
         </ul>
       </section>
-      {/*<div className="w-[95%] size671:w-[98%] size1090:w-[95%] m-auto flex flex-col size1056:flex-row gap-[1rem] size1275:gap-[3rem]">*/}
-      {/*  /!*<div className="hidden size1000:block size720:w-[48%] size1136:w-[45%] size1000:pt-[9rem] size1056:pt-0 mt-[1rem] self-center size1056:self-auto">*!/*/}
-      {/*  /!*  <SelectVehicleBox*!/*/}
-      {/*  /!*    tabTitle={tabTitle}*!/*/}
-      {/*  /!*    title="انتخاب وسیله نقلیه"*!/*/}
-      {/*  /!*    cityProvincesTitle={*!/*/}
-      {/*  /!*      "برای ثبت کارشناسی استان و شهر خود را انتخاب کنید"*!/*/}
-      {/*  /!*    }*!/*/}
-      {/*  /!*    setSelectedItem={setSelectedItem}*!/*/}
-      {/*  /!*    selectedItem={selectedItem}*!/*/}
-      {/*  /!*    setStep={setStep}*!/*/}
-      {/*  /!*    searchParams={props.searchParams}*!/*/}
-      {/*  /!*    step={step}*!/*/}
-      {/*  /!*  />*!/*/}
-      {/*  /!*</div>*!/*/}
-      {/*  <div className="pt-0 size1056:pt-[9rem] w-full mt-[1rem] flex flex-col gap-[1.5rem] relative">*/}
-      {/*    /!*<div className="w-full absolute top-0 right-0  hidden size1000:flex flex-row-reverse items-center gap-[1rem]">*!/*/}
-      {/*    /!*  <CarServicesSlider data={serviceData} />*!/*/}
-      {/*    /!*</div>*!/*/}
-      {/*    <div className="flex items-center justify-between">*/}
-      {/*      <div>*/}
-      {/*        <div*/}
-      {/*          ref={filterRef}*/}
-      {/*          onClick={toggleFilterHandler}*/}
-      {/*          className="bg-[#D9D9D9] rounded-[8px] py-[0.5rem] px-[1.5rem] flex items-center gap-[0.5rem] text-text_gray cursor-pointer relative"*/}
-      {/*        >*/}
-      {/*          <Image src={arrow} alt="" width={10} height={10} />*/}
-      {/*          <p className="text-12">{filter}</p>*/}
-      {/*          <ul*/}
-      {/*            ref={heightRef}*/}
-      {/*            className={`bg-[#D9D9D9] rounded-[8px] absolute  top-[2.2rem] right-0 left-0 text-12 overflow-hidden transition-all duration-500`}*/}
-      {/*            style={*/}
-      {/*              filterISOpen*/}
-      {/*                ? { height: `${heightRef.current.scrollHeight}px` }*/}
-      {/*                : { height: 0 }*/}
-      {/*            }*/}
-      {/*          >*/}
-      {/*            {filterData.map((item, index) => (*/}
-      {/*              <li*/}
-      {/*                className="p-[0.5rem] w-full rounded-[8px] hover:bg-[#9d9d9d] flex items-center justify-between"*/}
-      {/*                key={index}*/}
-      {/*                onClick={selectFilterHandler}*/}
-      {/*                onMouseEnter={filterMouseEnter}*/}
-      {/*                onMouseLeave={filterMouseLeave}*/}
-      {/*                order_by={item.value}*/}
-      {/*                id={item.value}*/}
-      {/*              >*/}
-      {/*                <span>{item.name}</span>*/}
-      {/*                <i className={"cc-left text-[20px]"} />*/}
-      {/*              </li>*/}
-      {/*            ))}*/}
-      {/*          </ul>*/}
-      {/*          {filterModalState && (*/}
-      {/*            <div*/}
-      {/*              className={*/}
-      {/*                "bg-white rounded-[8px] absolute right-[137px] pr-1 text-12 overflow-hidden transition-all duration-1000 w-[137px]"*/}
-      {/*              }*/}
-      {/*              ref={subFilterRef}*/}
-      {/*              id={"sub_filter"}*/}
-      {/*              onMouseEnter={subFilterMouseEnter}*/}
-      {/*              onMouseLeave={subFilterMouseLeave}*/}
-      {/*              style={{*/}
-      {/*                top: topDistance === 0 ? "0" : topDistance - 236 + "px",*/}
-      {/*              }}*/}
-      {/*            >*/}
-      {/*              <ul className={`bg-[#D9D9D9] h-[250px] overflow-y-scroll`}>*/}
-      {/*                {props.filterData[filterId]?.map((item) => (*/}
-      {/*                  <SubFilterCard*/}
-      {/*                    key={item.value}*/}
-      {/*                    filterId={filterId}*/}
-      {/*                    item={item}*/}
-      {/*                    setFilterModalState={setFilterModalState}*/}
-      {/*                  />*/}
-      {/*                ))}*/}
-      {/*              </ul>*/}
-      {/*            </div>*/}
-      {/*          )}*/}
-      {/*        </div>*/}
-      {/*      </div>*/}
-      {/*      /!* <Button class_name="bg-[#D9D9D9] rounded-[8px] py-[0.5rem] px-[1.5rem] flex items-center gap-[0.5rem] text-text_gray">*/}
-      {/*        <Image src={compare} alt="" width={15} height={15} />*/}
-      {/*        <p className="text-12">مقایسه محصولات</p>*/}
-      {/*      </Button> *!/*/}
-      {/*    </div>*/}
-      {/*    {isLoading ? (*/}
-      {/*      <div className={"flex justify-center items-center h-[100px]"}>*/}
-      {/*        <Spinner width={"w-[44px]"} height={"h-[44px]"} />*/}
-      {/*      </div>*/}
-      {/*    ) : (*/}
-      {/*      <ul className="flex flex-col gap-[1.5rem]">*/}
-      {/*        {data &&*/}
-      {/*          data.map((item) => (*/}
-      {/*            <BatteryCard*/}
-      {/*              key={item.id}*/}
-      {/*              setBatteryIsSelected={setBatteryIsSelected}*/}
-      {/*              data={item}*/}
-      {/*              searchParams={props.searchParams}*/}
-      {/*            />*/}
-      {/*          ))}*/}
-      {/*      </ul>*/}
-      {/*    )}*/}
-      {/*  </div>*/}
-      {/*  <div*/}
-      {/*    onClick={() => {*/}
-      {/*      setBatteryIsSelected(false);*/}
-      {/*    }}*/}
-      {/*    className={` w-full transition-all duration-500 ${batteryIsSelected ? "bg-[#000] opacity-[0.8] h-[100vh]" : "opacity-0 h-0"} fixed top-0 right-0 left-0 bottom-0 z-[9999]`}*/}
-      {/*  ></div>*/}
-      {/*  <div*/}
-      {/*    className={`w-[75%] size900:w-[50%] m-auto fixed transition-all duration-1000 ${batteryIsSelected ? "top-[50%]" : "top-[-50%]"} left-[50%] translate-x-[-50%] translate-y-[-50%] z-[99999]`}*/}
-      {/*  >*/}
-      {/*    <PurchaseBatteryModal*/}
-      {/*      setBatteryIsSelected={setBatteryIsSelected}*/}
-      {/*      batteryIsSelected={batteryIsSelected}*/}
-      {/*      searchParams={props.searchParams}*/}
-      {/*    />*/}
-      {/*  </div>*/}
-      {/*</div>*/}
       <div
         className={`${!batteryIsSelected ? "hidden" : "fixed"} inset-0 h-full w-full bg-[#4c4c4caa] z-[20000] transition-all`}
         onClick={() => setBatteryIsSelected(false)}
       ></div>
-
       <div
         className={`w-[75%] size900:w-[50%] m-auto fixed transition-all duration-1000 ${batteryIsSelected ? "top-[50%]" : "top-[-60%]"} left-[50%] translate-x-[-50%] translate-y-[-50%] z-[20000]`}
       >
