414431fd2ded07ace25009fa111bf93cbd74ba30
peyman-norozy <peyman.norozy@gmail.com>
2024-09-03 15:59:47 +0330

    delete margin top

--- app/detailing/selectLocation/page.jsx
+++ app/detailing/selectLocation/page.jsx
@@ -41,7 +41,7 @@ const Dealership = () => {
 
   console.log(carCheckLocations);
   return (
-    <div className="lg:flex items-start gap-8 mt-1 lg:mt-[108px] max-w-[1772px] m-auto py-4 relative">
+    <div className="lg:flex items-start gap-8 mt-3 max-w-[1772px] m-auto py-4 relative">
       <div
         className={
           "w-full lg:w-[calc(100%-424px)] md:gap-10 mr-auto flex flex-col gap-6"
