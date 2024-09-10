8ebcf55cbacabdf90427624969d11c8afaef8889
farshad <farshad.fsn@gmail.com>
2024-09-10 11:40:02 +0330

    edit price style

--- components/vehicle-verification/PackageCard.jsx
+++ components/vehicle-verification/PackageCard.jsx
@@ -29,10 +29,10 @@ const PackageCard = (props) => {
           <h6 className={"text-14 size752:text-16 text-[#010101]"}>{title}</h6>
         </div>
         <div className="flex items-center gap-1">
-          <p className={"text-[#8cb5d8] text-10 size752:text-16 line-through"}>
+          <p className={"text-[#60ABEC] font-medium text-xs size752:text-16 line-through"}>
             {price.toLocaleString()} تومان
           </p>
-          <p className={"text-[#518DD5] text-14 size752:text-16 font-medium"}>
+          <p className={"text-[#1E67BF] text-14 size752:text-16 font-medium"}>
             {discounted_price.toLocaleString()} تومان
           </p>
         </div>
