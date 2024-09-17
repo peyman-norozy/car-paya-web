346a6b59776e56bb8e79563f242d374a4cacb35e
peyman-norozy <peyman.norozy@gmail.com>
2024-09-17 17:34:24 +0330

    handle the bread crump

--- app/batteries/products/newSelectLocation/page.jsx
+++ app/batteries/products/newSelectLocation/page.jsx
@@ -5,7 +5,7 @@ import SelectLocationTab from "@/components/SelectLocationTab/SelectLocationTab"
 import AddressSelection from "@/components/AddressSelection/AddressSelection";
 import { useSelector } from "react-redux";
 import { getDataWithFullErrorRes } from "@/utils/api-function-utils";
-import { useSearchParams } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 
 const Page = (props) => {
   const [selectAddressState, setSelectAddressState] = useState("MOVING"); //FIXED
@@ -13,6 +13,12 @@ const Page = (props) => {
   const [carCheckLocations, setCarCheckLocations] = useState([]);
   const [filter, setFilter] = useState([]);
   const searchParams = useSearchParams();
+  const attributeSlug = searchParams.get("attribute_slug");
+  const attributeValue = searchParams.get("attribute_value");
+  const selectTipState = searchParams.get("selectTipState");
+  const amper = searchParams.get("amper");
+  const typeService = searchParams.get("type_service");
+  const router = useRouter();
 
   const showHeaderState = useSelector((state) => state.todo.showHeader);
 
@@ -45,8 +51,45 @@ const Page = (props) => {
 
   return (
     <div className={"min-h-screen mt-4 lg:mr-[470px] px-3 mb-[71px]"}>
+      <div
+        className={
+          "flex items-center gap-2 size752:gap-[16px] text-[#0E0E0E] w-full"
+        }
+      >
+        <i
+          className={"cc-arrow-right text-24 cursor-pointer"}
+          // onClick={backStepHandler}
+        />
+        <p className={"text-14 size752:text-16 w-full font-medium"}>
+          انتخاب زمان
+        </p>
+      </div>
+      <div className="flex gap-2 items-center w-full bg-[#FFFFFF] text-[#D1D1D1] border border-[#F2F2F2] rounded-full px-2 my-4">
+        <i
+          className="cc-car-o text-2xl text-[#1E67BF]"
+          onClick={() =>
+            router.push(
+              `/batteries?attribute_slug=${attributeSlug}&attribute_value=${attributeValue}&selectTipState=${selectTipState}`,
+            )
+          }
+        />
+        <div className="border-b-4 border-dotted border-[#1E67BF] w-full"></div>
+        <i
+          className="cc-search text-2xl text-[#1E67BF]"
+          onClick={() =>
+            router.push(
+              `/batteries/products?attribute_slug=${attributeSlug}&attribute_value=${attributeValue}&selectTipState=${selectTipState}&amper=${amper}&type_service=${typeService}`,
+            )
+          }
+        />
+        <div className="border-b-4 border-dotted border-[#1E67BF] w-full"></div>
+        <i className="cc-location text-2xl text-[#D1D1D1]" />
+        <div className="border-b-4 border-dotted border-[#D1D1D1] w-full"></div>
+        <i className="cc-timer text-2xl text-[#D1D1D1]" />
+      </div>
+      <span className={"font-semibold text-14"}>آدرس خود را انتخاب کنید:</span>
       <div className={`z-[2000] py-4 transition-all`}>
-        <div className={"flex justify-center sm:gap-[139px] gap-[20px]"}>
+        <div className={"flex justify-center"}>
           <SelectLocationTab
             headerText={"در محل شما"}
             addressTabState={"MOVING"}
