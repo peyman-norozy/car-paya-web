a92cc6f4ee9cce742b072b9eb648131616a870a5
farshad <farshad.fsn@gmail.com>
2024-09-17 12:47:49 +0330

    handle validation for otp input

--- components/login/LoginModal.jsx
+++ components/login/LoginModal.jsx
@@ -14,9 +14,11 @@ const LoginModal = () => {
   const [step, setStep] = useState(0);
   const [otp, setOtp] = useState("");
   const [isClient, setIsClient] = useState(false);
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const isOpen = useSelector((state) => state.todo.LoginModalState);
-  const [validation , setValidation] = useState(null)
+  const [phoneValidation, setPhoneValidation] = useState(null);
+  const [otpValidation, setOtpValidation] = useState(null);
+
   useEffect(() => {
     setIsClient(true);
   }, []);
@@ -31,11 +33,12 @@ const LoginModal = () => {
       .then((res) => {
         setLoginToken(res.data.data.login_token);
         setStep(1);
-      }).catch((err)=>{
-        if(err?.response?.status){
-          setValidation(err?.response?.data?.message)
-        }
       })
+      .catch((err) => {
+        if (err?.response?.status) {
+          setPhoneValidation(err?.response?.data?.message);
+        }
+      });
   }
   function sendOtpHandler() {
     axios
@@ -51,7 +54,11 @@ const LoginModal = () => {
         setOtp("");
         setLoginToken("");
         setPhone("");
-        closeModal()
+        closeModal();
+      }).catch((err) => {
+        if (err?.response?.status) {
+          setOtpValidation(err?.response?.data?.message);
+        }
       });
   }
 
@@ -68,18 +75,34 @@ const LoginModal = () => {
         onClick={() => closeModal()}
       >
         {step === 0 ? (
-          <div className="absolute bottom-0 right-0 sm:inset-0 sm:m-auto h-fit sm:max-w-[560px] sm:rounded-2xl w-screen p-7 flex flex-col items-center gap-10 z-[3001] bg-white shadow-[0_0_8px_0_rgba(175,175,175,0.25)] rounded-t-3xl pb-32 sm:p-10" onClick={(event)=>{event.stopPropagation()}}>
+          <div
+            className="absolute bottom-0 right-0 sm:inset-0 sm:m-auto h-fit sm:max-w-[560px] sm:rounded-2xl w-screen p-7 flex flex-col items-center gap-10 z-[3001] bg-white shadow-[0_0_8px_0_rgba(175,175,175,0.25)] rounded-t-3xl pb-32 sm:p-10"
+            onClick={(event) => {
+              event.stopPropagation();
+            }}
+          >
             <span className="text-2xl text-[#F66B34] font-black">CAR PAYA</span>
-            <i className="cc-add text-2xl rotate-45 absolute top-5 left-5" onClick={closeModal}/>
+            <i
+              className="cc-add text-2xl rotate-45 absolute top-5 left-5"
+              onClick={closeModal}
+            />
             <span className="font-medium">ورود/ ثبت نام</span>
             <div className="flex flex-col items-center gap-6 w-full">
               <div className="flex flex-col items-start gap-4 w-full">
                 <span className="font-medium text-xs mr-4 w-full">
                   لطفا شماره موبایل خود را وارد کنید
                 </span>
-                <TextInput label="شماره تماس" value={phone} icon={"cc-user"} disabled={false} validation={validation} placeholder={""} onChange={(e) => {
-            setPhone(e.target.value);
-          }} />
+                <TextInput
+                  label="شماره تماس"
+                  value={phone}
+                  icon={"cc-user"}
+                  disabled={false}
+                  validation={phoneValidation}
+                  placeholder={""}
+                  onChange={(e) => {
+                    setPhone(e.target.value);
+                  }}
+                />
               </div>
               <div className="flex flex-col gap-2 w-full items-center">
                 <button
@@ -97,16 +120,24 @@ const LoginModal = () => {
             </div>
           </div>
         ) : (
-          <div className="absolute bottom-0 right-0 sm:inset-0 sm:m-auto h-fit sm:max-w-[560px] sm:rounded-2xl w-screen p-7 flex flex-col items-center gap-10 z-[3001] bg-white shadow-[0_0_8px_0_rgba(175,175,175,0.25)] rounded-t-3xl pb-32 sm:p-10" onClick={(event)=>{event.stopPropagation()}}>
+          <div
+            className="absolute bottom-0 right-0 sm:inset-0 sm:m-auto h-fit sm:max-w-[560px] sm:rounded-2xl w-screen p-7 flex flex-col items-center gap-10 z-[3001] bg-white shadow-[0_0_8px_0_rgba(175,175,175,0.25)] rounded-t-3xl pb-32 sm:p-10"
+            onClick={(event) => {
+              event.stopPropagation();
+            }}
+          >
             <span className="text-2xl text-[#F66B34] font-black">CAR PAYA</span>
-            <i className="cc-add text-2xl rotate-45 absolute top-5 left-5" onClick={closeModal}/>
+            <i
+              className="cc-add text-2xl rotate-45 absolute top-5 left-5"
+              onClick={closeModal}
+            />
             <span className="font-medium">ورود/ ثبت نام</span>
             <div className="flex flex-col items-center gap-6 w-full rounded">
               <div className="flex items-center flex-col w-full gap-2">
                 <span className="text-xs font-medium">
                   کد تایید برای شماره {phone} پیامک شد
                 </span>
-                <div className="m-auto">
+                <div className="m-auto flex flex-col w-fit items-start gap-2">
                   <OTPInput
                     value={otp}
                     onChange={setOtp}
@@ -122,10 +153,13 @@ const LoginModal = () => {
                       marginLeft: "10px",
                       gap: "10px",
                     }}
-                    inputStyle={
-                      "flex-1 h-full bg-[#00000000] outline-none border border-[#B0B0B0] text-14 font-medium rounded-[4px]"
-                    }
+                    inputStyle={`flex-1 h-full bg-[#00000000] outline-none border ${otpValidation ? "border-[#FF0031]" : "border-[#B0B0B0]"} text-[#3C3C3C] text-14 font-medium rounded-[4px]`}
                   />
+                  {otpValidation && (
+                    <span className="text-[#FF0031] px-1 text-14">
+                      {otpValidation}
+                    </span>
+                  )}
                 </div>
               </div>
               <div className="flex flex-col gap-2 w-full items-center">
