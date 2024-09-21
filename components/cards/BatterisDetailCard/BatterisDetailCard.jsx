cadd6f2be785689570cfcaa25a8601435bed4a81
peyman-norozy <peyman.norozy@gmail.com>
2024-09-21 09:45:30 +0330

    add teh tick icon and change style

--- components/cards/BatterisDetailCard/BatterisDetailCard.jsx
+++ components/cards/BatterisDetailCard/BatterisDetailCard.jsx
@@ -4,46 +4,70 @@ const BatterisDetailCard = (props) => {
   return (
     <>
       <li className={"flex items-center gap-[8px] w-full max-w-[100px]"}>
-        <span
-          className={`inline-block ${props.buletStyle ? props.buletStyle : "w-[20px] h-[20px] bg-[#24D34B80]"} rounded-full`}
-        ></span>
-        <p>آمپر:</p>
-        <p>{props.item.amp}</p>
+        <i
+          className={
+            "cc-tick bg-[#DDF5E9] text-[#34B674] rounded-full py-[2px] px-[1px] text-[14px]"
+          }
+        />
+        <p className={"text-[12px] lg:text-[14px] font-medium"}>آمپر:</p>
+        <p className={"text-[12px] lg:text-[14px] font-medium"}>
+          {props.item.amp}
+        </p>
       </li>
       <li className={"flex items-center gap-[8px] w-full max-w-[100px]"}>
-        <span
-          className={`inline-block ${props.buletStyle ? props.buletStyle : "w-[20px] h-[20px] bg-[#24D34B80]"} rounded-full`}
-        ></span>
-        <p>تکنولوژی:</p>
-        <p>{props.item.filters.technology}</p>
+        <i
+          className={
+            "cc-tick bg-[#DDF5E9] text-[#34B674] rounded-full py-[2px] px-[1px] text-[14px]"
+          }
+        />
+        <p className={"text-[12px] lg:text-[14px] font-medium"}>تکنولوژی:</p>
+        <p className={"text-[12px] lg:text-[14px] font-medium"}>
+          {props.item.filters.technology}
+        </p>
       </li>
       <li className={"flex items-center gap-[8px] w-full max-w-[100px]"}>
-        <span
-          className={`inline-block ${props.buletStyle ? props.buletStyle : "w-[20px] h-[20px] bg-[#24D34B80]"} rounded-full`}
-        ></span>
-        <p>ولتاژ:</p>
-        <p>{props.item.filters.voltage}</p>
+        <i
+          className={
+            "cc-tick bg-[#DDF5E9] text-[#34B674] rounded-full py-[2px] px-[1px] text-[14px]"
+          }
+        />
+        <p className={"text-[12px] lg:text-[14px] font-medium"}>ولتاژ:</p>
+        <p className={"text-[12px] lg:text-[14px] font-medium"}>
+          {props.item.filters.voltage}
+        </p>
       </li>
       <li className={"flex items-center gap-[8px] w-full max-w-[100px]"}>
-        <span
-          className={`inline-block ${props.buletStyle ? props.buletStyle : "w-[20px] h-[20px] bg-[#24D34B80]"} rounded-full`}
-        ></span>
-        <p>قطب:</p>
-        <p>{props.item.filters.pole}</p>
+        <i
+          className={
+            "cc-tick bg-[#DDF5E9] text-[#34B674] rounded-full py-[2px] px-[1px] text-[14px]"
+          }
+        />
+        <p className={"text-[12px] lg:text-[14px] font-medium"}>قطب:</p>
+        <p className={"text-[12px] lg:text-[14px] font-medium"}>
+          {props.item.filters.pole}
+        </p>
       </li>
       <li className={"flex items-center gap-[8px] w-full max-w-[100px]"}>
-        <span
-          className={`inline-block ${props.buletStyle ? props.buletStyle : "w-[20px] h-[20px] bg-[#24D34B80]"} rounded-full`}
-        ></span>
-        <p>گارانتی:</p>
-        <p>{props.item.filters.warranty}</p>
+        <i
+          className={
+            "cc-tick bg-[#DDF5E9] text-[#34B674] rounded-full py-[2px] px-[1px] text-[14px]"
+          }
+        />
+        <p className={"text-[12px] lg:text-[14px] font-medium"}>گارانتی:</p>
+        <p className={"text-[12px] lg:text-[14px] font-medium"}>
+          {props.item.filters.warranty}
+        </p>
       </li>
       <li className={"flex items-center gap-[8px] w-full max-w-[100px]"}>
-        <span
-          className={`inline-block ${props.buletStyle ? props.buletStyle : "w-[20px] h-[20px] bg-[#24D34B80]"} rounded-full`}
-        ></span>
-        <p>نوع:</p>
-        <p>{props.item.filters.type}</p>
+        <i
+          className={
+            "cc-tick bg-[#DDF5E9] text-[#34B674] rounded-full py-[2px] px-[1px] text-[14px]"
+          }
+        />
+        <p className={"text-[12px] lg:text-[14px] font-medium"}>نوع:</p>
+        <p className={"text-[12px] lg:text-[14px] font-medium"}>
+          {props.item.filters.type}
+        </p>
       </li>
     </>
   );
