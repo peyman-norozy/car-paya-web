c827c7ada14dbe6b2f8ca04cb225d36ef62e057a
peyman-norozy <peyman.norozy@gmail.com>
2024-09-23 11:45:26 +0330

    handle the battery landing page

--- components/BatteriesMainPage/BatteriesMainPage.jsx
+++ components/BatteriesMainPage/BatteriesMainPage.jsx
@@ -11,6 +11,7 @@ import BatteryAdvice from "@/components/‌BatteryAdvice/‌BatteryAdvice";
 import { error } from "@/utils/function-utils";
 import { usePathname, useSearchParams } from "next/navigation";
 import CarServicesSlider from "@/components/CarServicesSlider/CarServicesSlider";
+import PointView from "@/components/PointView/PointView";
 
 const BatteriesMainPage = () => {
   const [client, setClient] = useState(false);
@@ -53,14 +54,46 @@ const BatteriesMainPage = () => {
 
   return (
     <>
-      <div className={"bg-[#EBF5FF] rounded-[16px] mt-6"}>
-        <Image
-          src={"/assets/images/batteryBanner.png"}
-          alt={"battery banner"}
-          width={1296}
-          height={500}
-          className={"w-full h-[615px]"}
-        />
+      <div
+        className={
+          "bg-[#EBF5FF] rounded-[16px] mt-6 w-full h-auto aspect-[full/612] flex flex-col"
+        }
+      >
+        <h1
+          className={
+            "flex items-center justify-center gap-1 lg:text-[24px] text-[20px] font-bold mt-10 mr-8 lg:mr-0 self-start lg:self-center"
+          }
+        >
+          <span>استارت مطمئن با</span>
+          <span className={"text-[#F66B34]"}>کارپایا</span>
+        </h1>
+        <div className={"flex flex-row-reverse justify-start"}>
+          <Image
+            src={"/assets/images/carshenasi.png"}
+            alt={"battery banner"}
+            width={522}
+            height={352}
+            className={
+              "lg:w-[522px] lg:h-[352px] w-[224px] h-[151px] self-end mb-[71px]"
+            }
+          />
+          <Image
+            src={"/assets/images/image 110 (1).svg"}
+            alt={"battery banner"}
+            width={178}
+            height={217}
+            className={
+              "lg:w-[178px] lg:h-[217px] w-[82px] h-[100px] self-end mb-[71px]"
+            }
+          />
+        </div>
+        <button
+          className={
+            "block lg:hidden bg-[#F66B34] text-[#FEFEFE] w-[165px] h-[36px] mb-[14px] text-[12px] font-medium self-center rounded-[4px]"
+          }
+        >
+          درخواست باتری
+        </button>
       </div>
       <CarServicesSlider data={serviceData} />
       <div
@@ -68,9 +101,9 @@ const BatteriesMainPage = () => {
           "flex flex-col relative max-w-[1772px] m-auto lg:mr-[424px] pb-10 mt-10"
         }
       >
-        <h1 className={"text-[18px] font-medium text-[#F58052]"}>
+        <h2 className={"text-[18px] font-medium text-[#F58052]"}>
           مراحل تعویض باتری با کار پایا
-        </h1>
+        </h2>
         <ul
           className={
             "flex justify-between lg:gap-8 gap-4 mt-10 overflow-x-scroll py-2 px-1 [&>*:last-child]:hidden"
@@ -80,7 +113,9 @@ const BatteriesMainPage = () => {
             <BatteryPurchaseProcess key={index} {...item} />
           ))}
         </ul>
-        <BatteryAdvice />
+        <PointView />
+
+        {/*<BatteryAdvice />*/}
         <BatteryFaq />
         {preventFirstRender && <ToastContainer rtl={true} />}
       </div>
