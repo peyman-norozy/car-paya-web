74e631d6b99b0f120136f3b21874fcffb8d73dc8
peyman-norozy <peyman.norozy@gmail.com>
2024-09-25 17:51:31 +0330

    add the new className

--- components/public/SelectCity.jsx
+++ components/public/SelectCity.jsx
@@ -28,7 +28,7 @@ const SelectCity = (props) => {
   return (
     <section
       className={
-        "bg-[#FDFDFD] w-[409px] h-[485px] sticky right-2 top-32 rounded-[16px] flex flex-col justify-between overflow-hidden gap-4 shadow-[0_0_6px_6px_rgba(125,125,125,0.5)] z-50"
+        "bg-[#FDFDFD] lg:w-[409px] w-full h-[485px] sticky right-2 top-32 rounded-[16px] pt-6 flex flex-col justify-between overflow-hidden gap-4 shadow-[0_0_6px_6px_rgba(125,125,125,0.5)] z-50"
       }
     >
       <ul className={"grid grid-cols-3 gap-y-9 px-4 py-4"}>
