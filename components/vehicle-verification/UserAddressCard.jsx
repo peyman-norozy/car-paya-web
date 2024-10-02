5ebfa96c5aeb4f4d6ebb1e6babbd3827a9752eca
farshad <farshad.fsn@gmail.com>
2024-10-02 12:15:59 +0330

    fix verification routing

--- components/vehicle-verification/UserAddressCard.jsx
+++ components/vehicle-verification/UserAddressCard.jsx
@@ -8,9 +8,9 @@ const UserAddressCard = (props) => {
   const dispatch = useDispatch();
   return (
     <div
-      className={`flex flex-col gap-4 p-4 rounded-lg bg-white cursor-pointer shadow-[0_0_4px_0_rgba(207,207,207,0.7)] ${props.selectedAddress === props.data.id ? "border border-[#F58052]" : ""}`}
+      className={`flex flex-col gap-4 p-4 rounded-lg bg-white cursor-pointer shadow-[0_0_4px_0_rgba(207,207,207,0.7)] ${props.selectedAddress === props.data.address_id ? "border border-[#F58052]" : ""}`}
       onClick={() => {
-        props.selectedAddress === props.data.id
+        props.selectedAddress === props.data.address_id
           ? props.setSelectedAddress("")
           : props.setSelectedAddress(props.data.address_id);
       }}
@@ -23,7 +23,7 @@ const UserAddressCard = (props) => {
             }
           >
             <div
-              className={`rounded-[50%] bg-[#F58052] size-[10px] transition-all duration-500 ${props.selectedAddress === props.data.id ? "scale-1" : "scale-0"}`}
+              className={`rounded-[50%] bg-[#F58052] size-[10px] transition-all duration-500 ${props.selectedAddress === props.data.address_id ? "scale-1" : "scale-0"}`}
             ></div>
           </div>
           <span className="text-[#000000] text-sm">{props.data.title}</span>
