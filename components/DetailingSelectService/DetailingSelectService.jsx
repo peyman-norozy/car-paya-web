ec6e1f9d65069adee066e286c7983624b10251b9
peyman-norozy <peyman.norozy@gmail.com>
2024-10-07 16:50:23 +0330

    fixed the detailing without faktor page

--- components/DetailingSelectService/DetailingSelectService.jsx
+++ components/DetailingSelectService/DetailingSelectService.jsx
@@ -4,19 +4,24 @@ import React, { useState } from "react";
 import { ToastContainer } from "react-toastify";
 import SelectLocationTab from "@/components/SelectLocationTab/SelectLocationTab";
 import AddressSelection from "@/components/AddressSelection/AddressSelection";
-import { useRouter, useSearchParams } from "next/navigation";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { numberWithCommas } from "@/utils/function-utils";
+import useSetQuery from "@/hook/useSetQuery";
 
 const DetailingSelectService = (props) => {
   console.log(props.data);
   const [productId, setProductId] = useState(null);
+  const [ditailingPrice, setDitailingPrice] = useState(0);
 
   const [childFunction, setChildFunction] = useState(null);
-
-  const handleChildFunction = (func) => {
-    setChildFunction(() => func);
-  };
+  const pathName = usePathname();
+  const setQuery = useSetQuery();
   const router = useRouter();
   const searchParams = useSearchParams();
+
+  const buttonClickHandler = (id) => {
+    setQuery.updateQueryParams({ package_id: id }, "/detailing/timeSelector");
+  };
 
   const attributeSlug = searchParams.get("attribute_slug");
   const attributeValue = searchParams.get("attribute_value");
@@ -24,12 +29,14 @@ const DetailingSelectService = (props) => {
   const typeService = searchParams.get("type_service");
   const cityId = searchParams.get("city_id");
   const type = searchParams.get("type");
+  const serviceLocationId = searchParams.get("service_location_id");
 
-  // ?type=FIXED&attribute_slug=type_vehicle&attribute_value=car&selectTipState=true%2C16&city_id=87
+  console.log(props);
+
   return (
     <div
       className={
-        "flex flex-col relative py-4 max-w-[1772px] lg:w-[calc(100%-424px)] mr-auto bg-[#FDFDFD] lg:shadow-[0_0_6px_0_rgba(125,125,125,0.5)] lg:p-6 rounded-2xl min-h-[605px] mb-4 lg:mt-7"
+        "flex flex-col relative py-4 max-w-[1772px] lg:w-[calc(100%-424px)] w-full lg:mr-auto mr-0 bg-[#FDFDFD] lg:shadow-[0_0_6px_0_rgba(125,125,125,0.5)] lg:p-6 rounded-2xl min-h-[605px] mb-4 lg:mt-7"
       }
     >
       <div
@@ -41,7 +48,7 @@ const DetailingSelectService = (props) => {
           className={"cc-arrow-right text-24 cursor-pointer"}
           onClick={() =>
             router.push(
-              `/detailing/selectLocation?type=${type}&attribute_slug=${attributeSlug}&attribute_value=${attributeValue}&selectTipState=${selectTipState}&city_id=${cityId}`,
+              `/detailing/selectLocation?type=${type}&selectTipState=${selectTipState}&city_id=${cityId}&service_location_id=${serviceLocationId}`,
             )
           }
         />
@@ -84,9 +91,9 @@ const DetailingSelectService = (props) => {
       >
         <span className={"text-14 font-medium"}>نمایندگی ایران خودرو</span>
       </div>
-      <div className={"py-4 flex flex-col h-full justify-between"}>
-        <div className={"h-[calc(100%-100px)] overflow-y-scroll"}>
-          <div className="grid lg:grid-cols-2 grid-cols-1 sm:gap-2 lg:gap-4 h-fit">
+      <div className={"py-4 flex flex-col h-full justify-between relative"}>
+        <div className={"lg:h-[calc(100%-100px)] overflow-y-scroll"}>
+          <div className="grid size1570:grid-cols-3 size1275:grid-cols-2 grid-cols-1 sm:gap-2 lg:gap-4 gap-4 h-fit">
             {props.data?.map((item, index) => (
               <DetailingSelectServiceCard
                 data={item}
@@ -94,19 +101,33 @@ const DetailingSelectService = (props) => {
                 setProductId={setProductId}
                 productId={productId}
                 key={index}
-                sendToParent={handleChildFunction}
+                setDitailingPrice={setDitailingPrice}
+                // sendToParent={handleChildFunction}
               />
             ))}
           </div>
         </div>
-        <button
+        <div
           className={
-            "bg-[#F66B34] text-white text-14 self-end p-2 rounded-5 mt-5 sticky bottom-0"
+            "lg:sticky fixed flex justify-between items-center bottom-0 right-0 left-0 z-50 bg-[#FFFFFF] h-[75px] mx-4"
           }
-          onClick={async () => await childFunction(productId)}
         >
-          تایید و تکمیل سفارش
-        </button>
+          <div className={"flex lg:flex-row flex-col items-center gap-1"}>
+            <span className={"text-16 font-medium"}>مبلغ سفارش:</span>
+            <div>
+              <span className={"text-[#518DD5] text-14"}>
+                {numberWithCommas(ditailingPrice)}
+              </span>
+              <span className={"text-[#518DD5] text-14"}>تومان</span>
+            </div>
+          </div>
+          <button
+            className={"bg-[#F66B34] text-white text-16 py-2 px-8 rounded-5"}
+            onClick={() => buttonClickHandler(productId)}
+          >
+            تایید و تکمیل سفارش
+          </button>
+        </div>
       </div>
       <ToastContainer rtl={true} />
     </div>
