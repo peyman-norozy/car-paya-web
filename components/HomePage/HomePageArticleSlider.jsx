11b0c8f37715e1758c8e8c34609086640b0d7f53
peyman-norozy <peyman.norozy@gmail.com>
2024-10-09 18:06:40 +0330

    add the item.permalink data

--- components/HomePage/HomePageArticleSlider.jsx
+++ components/HomePage/HomePageArticleSlider.jsx
@@ -6,12 +6,14 @@ import Image from "next/image";
 import { API_PATHS } from "@/configs/routes.config";
 import moment from "jalali-moment";
 import { EffectCoverflow } from "swiper/modules";
+import Link from "next/link";
 
 const HomePageArticleSlider = (props) => {
   moment.locale("fa");
   if (props?.data?.error?.code === 404) {
     return "";
   }
+  console.log(props);
   return (
     <div className="flex flex-col">
       <span className="text-sm sm:text-xl font-bold text-black text-start px-4 mb-3">
@@ -47,23 +49,25 @@ const HomePageArticleSlider = (props) => {
       >
         {props?.data?.map((item, index) => (
           <SwiperSlide style={{ width: "fit-content" }} key={item.id}>
-            <div className="rounded-lg md:rounded-[8px] overflow-hidden relative">
-              <Image
-                src={item["featured_image"]}
-                className="w-[172px] sm:w-[303px] h-[142px] sm:h-[220px]"
-                width={174}
-                height={142}
-                alt=""
-              />
-              <div className="absolute bottom-0 right-0 w-full h-[27%] shadow-[0_-2px_4px_0_rgba(227,227,227,0.35)] backdrop-blur-3xl bg-[#27262660] flex flex-col items-start gap-1 md:gap-2 py-1 md:py-2 px-2 md:px-4">
-                <span className="font-medium text-xs md:text-base text-[#FEFEFE] line-clamp-1">
-                  {item.title}
-                </span>
-                <p className="text-[#FEFEFE] text-[10px] md:text-sm line-clamp-3">
-                  {item.date}
-                </p>
+            <Link href={item.permalink}>
+              <div className="rounded-lg md:rounded-[8px] overflow-hidden relative">
+                <Image
+                  src={item["featured_image"]}
+                  className="w-[172px] sm:w-[303px] h-[142px] sm:h-[220px]"
+                  width={174}
+                  height={142}
+                  alt=""
+                />
+                <div className="absolute bottom-0 right-0 w-full h-[27%] shadow-[0_-2px_4px_0_rgba(227,227,227,0.35)] backdrop-blur-3xl bg-[#27262660] flex flex-col items-start gap-1 md:gap-2 py-1 md:py-2 px-2 md:px-4">
+                  <span className="font-medium text-xs md:text-base text-[#FEFEFE] line-clamp-1">
+                    {item.title}
+                  </span>
+                  <p className="text-[#FEFEFE] text-[10px] md:text-sm line-clamp-3">
+                    {item.date}
+                  </p>
+                </div>
               </div>
-            </div>
+            </Link>
           </SwiperSlide>
         ))}
       </Swiper>
