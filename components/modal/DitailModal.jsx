296037bbfe9c3143cf91aa2e9b2b2f6591051361
peyman-norozy <peyman.norozy@gmail.com>
2024-10-09 18:12:48 +0330

    make DitailModal.jsx and custom it

---
+++ components/modal/DitailModal.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+const DitailModal = (props) => {
+  console.log(props);
+
+  const closeModalHandler = () => {
+    props.setEditModalIsOpen(false);
+  };
+
+  return (
+    <>
+      <div
+        className={
+          "bg-[#4c4c4caa] opacity-50 fixed right-0 top-0 w-full h-full z-[10000]"
+        }
+      ></div>
+      <div
+        className={
+          "fixed m-auto inset-0 z-[10000] w-fit h-fit rounded-8 overflow-hidden"
+        }
+      >
+        <div
+          className={"bg-[#F6FBFF] p-[10px] flex justify-between items-center"}
+        >
+          <span className={"text-14"}>خدمات نمایندگی محمدی</span>
+          <i
+            className={"cc-close-circle text-24"}
+            onClick={closeModalHandler}
+          />
+        </div>
+        <ul
+          className={
+            "grid grid-cols-2 gap-x-[86px] gap-y-4 w-full bg-white px-5 pt-2 pb-3 text-[#47505D] text-14 font-medium"
+          }
+        >
+          {props.item?.map((item) => (
+            <li key={item.key} className={"flex items-center gap-2"}>
+              <i
+                className={
+                  "cc-tick text-[#24D34B] bg-[#24D34B40] w-[17px] h-[17px] rounded-full text-14 flex items-center justify-center"
+                }
+              />
+              <span>{item.label}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default DitailModal;
