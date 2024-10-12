d8d59f9765e8c47a5ce3f22a7ae7f14b938a5102
peyman-norozy <peyman.norozy@gmail.com>
2024-10-12 17:03:46 +0330

    change style and redesign

--- components/cards/DetailingSelectServiceCard/DetailingSelectServiceCard.jsx
+++ components/cards/DetailingSelectServiceCard/DetailingSelectServiceCard.jsx
@@ -22,70 +22,108 @@ const SelectServiceCard = (props) => {
     props.setProductId(props.data.id);
   };
 
-  console.log(props.productId);
+  console.log(props);
 
   return (
     <div
-      className={`rounded-lg flex items-start p-2 gap-3 cursor-pointer transition-transform duration-300 relative shadow-[0_0_4px_0_rgba(152,152,152,0.4)] ${props.data.id === props.productId ? "border-2 border-[#F66B34]" : ""}`}
+      className={`rounded-lg flex flex-col p-2 gap-1 cursor-pointer transition-transform duration-300 relative shadow-[0_0_4px_0_rgba(152,152,152,0.4)] ${props.data.id === props.productId ? "border-2 border-[#F66B34]" : ""}`}
       onClick={selectProductHandler}
     >
-      <section className={"flex gap-2"}>
-        <div
-          className={
-            "border-2 border-[#F66B34] size-[20px] rounded-full relative"
-          }
-        >
-          {props.data.id === props.productId && (
-            <div
+      <section className={"flex gap-2 justify-between"}>
+        <div>
+          <div className={"flex items-center gap-1"}>
+            <div>
+              <div
+                className={
+                  "border-2 border-[#F66B34] size-[20px] rounded-full relative"
+                }
+              >
+                {props.data.id === props.productId && (
+                  <div
+                    className={
+                      "bg-[#F66B34] size-3 rounded-full absolute top-[2px] right-[2px]"
+                    }
+                  ></div>
+                )}
+              </div>
+            </div>
+            <div className={"flex w-full"}>
+              <span className="text-14 lg:text-18 font-medium">
+                {props.data.selected ? props.data.product : props.data.name}
+              </span>
+            </div>
+          </div>
+          <div>
+            <ul
               className={
-                "bg-[#F66B34] size-3 rounded-full absolute top-[2px] right-[2px]"
+                "text-[12px] font-medium flex flex-col gap-2 list-disc pr-4 mt-2"
               }
-            ></div>
-          )}
+            >
+              <li>سرامیک خودرو / محافظ کننده خودرو</li>
+              <li>سرامیک خودرو / محافظ کننده خودرو</li>
+            </ul>
+          </div>
         </div>
-        <div className={"w-[76px] h-[65px]"}>
-          <Image
-            className="bg-amber-600 rounded-lg size-full"
-            src={
-              process.env.BASE_API +
-              "/web" +
-              API_PATHS.FILE +
-              "/" +
-              props.data.image_id
-            }
-            alt={"detailing service image"}
-            width={76}
-            height={65}
-          />
+        <div>
+          <div className={"w-[95px] h-[80px]"}>
+            <Image
+              className="bg-amber-600 rounded-lg size-full"
+              src={
+                process.env.BASE_API +
+                "/web" +
+                API_PATHS.FILE +
+                "/" +
+                props.data.image_id
+              }
+              alt={"detailing service image"}
+              width={95}
+              height={80}
+            />
+          </div>
         </div>
       </section>
       <section className={"flex-1"}>
-        <div className={"flex w-full"}>
-          <span className="text-14 lg:text-18 font-medium">
-            {props.data.selected ? props.data.product : props.data.name}
-          </span>
-        </div>
-        <ul className={"text-[12px] font-medium flex flex-col gap-2"}>
-          <li>سرامیک خودرو / محافظ کننده خودرو </li>
-          <li>سرامیک خودرو / محافظ کننده خودرو </li>
-        </ul>
         <div className={"w-full h-[1px] bg-[#BBBBBB] my-2"}></div>
-        {props.data?.discount_sallary && (
-          <div
-            className={
-              "flex items-center gap-1 line-through text-12 font-medium"
-            }
-          >
-            <span>{numberWithCommas(props.data?.discount_sallary)}</span>
-            <span className={"text-[12px] font-semibold"}>تومان</span>
+        {props.data?.discounted_percent ? (
+          <div className={"flex items-center gap-3"}>
+            <div
+              className={
+                "flex items-center gap-1 text-12 font-medium lg:text-14 text-12"
+              }
+            >
+              <span>قیمت :</span>
+              <div className={"flex items-center gap-1 text-12 font-medium"}>
+                <span>{numberWithCommas(props.data?.salary)}</span>
+                <span className={"text-[12px] font-semibold"}>تومان</span>
+              </div>
+            </div>
+            <div className={"line-through lg:text-14 text-12 text-[#888888]"}>
+              <span>{numberWithCommas(props.data?.discounted_salary)}</span>
+              <span className={"text-[12px] font-semibold"}>تومان</span>
+            </div>
           </div>
-        )}
-        {props.data?.salary && (
+        ) : (
           <div className={"flex items-center gap-1 text-12 font-medium"}>
             <span>{numberWithCommas(props.data?.salary)}</span>
             <span className={"text-[12px] font-semibold"}>تومان</span>
           </div>
         )}
+        {/*{props.data?.discount_salary && props.data?.discounted_percent && (*/}
+        {/*  <div*/}
+        {/*    className={*/}
+        {/*      "flex items-center gap-1 line-through text-12 font-medium"*/}
+        {/*    }*/}
+        {/*  >*/}
+        {/*    <span>{numberWithCommas(props.data?.discounted_salary)}</span>*/}
+        {/*    <span className={"text-[12px] font-semibold"}>تومان</span>*/}
+        {/*  </div>*/}
+        {/*)}*/}
+        {/*{props.data?.salary && (*/}
+        {/*  <div className={"flex items-center gap-1 text-12 font-medium"}>*/}
+        {/*    <span>{numberWithCommas(props.data?.salary)}</span>*/}
+        {/*    <span className={"text-[12px] font-semibold"}>تومان</span>*/}
+        {/*  </div>*/}
+        {/*)}*/}
       </section>
     </div>
   );
