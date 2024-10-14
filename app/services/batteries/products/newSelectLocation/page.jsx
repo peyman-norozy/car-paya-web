8c2607027a009a35ae93a960c912783046df019e
peyman-norozy <peyman.norozy@gmail.com>
2024-10-14 12:46:14 +0330

    add the ServiceInformation component and handle it

--- app/services/batteries/products/newSelectLocation/page.jsx
+++ app/services/batteries/products/newSelectLocation/page.jsx
@@ -7,10 +7,12 @@ import { useDispatch, useSelector } from "react-redux";
 import { getDataWithFullErrorRes } from "@/utils/api-function-utils";
 import { useRouter, useSearchParams } from "next/navigation";
 import { setAreaeModalState } from "@/store/todoSlice";
+import ServiceInformation from "@/components/ServiceInformation/ServiceInformation";
 
 const Page = (props) => {
   const [selectAddressState, setSelectAddressState] = useState("MOVING"); //FIXED
   const [locationId, setLocationId] = useState("");
+  const [client, setClient] = useState(false);
 
   const [myLocationData, setMyLocationData] = useState([]);
   const [carCheckLocations, setCarCheckLocations] = useState([]);
@@ -56,100 +58,123 @@ const Page = (props) => {
     timeData();
   }, [searchParams, renderUserAddrressState]);
 
+  useEffect(() => {
+    setClient(true);
+  }, []);
+
+  if (!client) {
+    return null;
+  }
+
+  const batteriesCart = JSON.parse(sessionStorage.getItem("batteriesCart"));
+
   return (
-    <div
-      className={
-        "flex flex-col relative py-4 max-w-[1772px] lg:w-[calc(100%-424px)] mr-auto bg-[#FDFDFD] lg:shadow-[0_0_6px_0_rgba(125,125,125,0.5)] lg:p-6 rounded-2xl min-h-[605px] mb-4 lg:mt-7"
-      }
-    >
-      <div
-        className={
-          "flex items-center gap-2 size752:gap-[16px] text-[#0E0E0E] w-full"
-        }
-      >
-        <i
-          className={"cc-arrow-right text-24 cursor-pointer"}
-          onClick={() =>
-            router.push(
-              `/services/batteries/products?attribute_slug=${attributeSlug}&attribute_value=${attributeValue}&selectTipState=${selectTipState}&amper=${amper}&type_service=${typeService}`,
-            )
-          }
-        />
-        <p className={"text-14 size752:text-16 w-full font-medium"}>
-          انتخاب مکان
-        </p>
-      </div>
-      <div
-        className={
-          "flex gap-2 items-center w-full bg-[#FFFFFF] text-[#D1D1D1] shadow-[0_0_4px_0_rgba(152,152,152,0.4)] lg:py-2 py-1 rounded-[16px] px-2 my-4"
-        }
-      >
-        <i
-          className="cc-car-o text-2xl text-[#1E67BF]"
-          onClick={() =>
-            router.push(
-              `/batteries?attribute_slug=${attributeSlug}&attribute_value=${attributeValue}&selectTipState=${selectTipState}`,
-            )
-          }
-        />
-        <div className="border-b-4 border-dotted border-[#1E67BF] w-full"></div>
-        <i
-          className="cc-search text-2xl text-[#1E67BF]"
-          onClick={() =>
-            router.push(
-              `/services/batteries/products?attribute_slug=${attributeSlug}&attribute_value=${attributeValue}&selectTipState=${selectTipState}&amper=${amper}&type_service=${typeService}`,
-            )
-          }
-        />
-        <div className="border-b-4 border-dotted border-[#1E67BF] w-full"></div>
-        <i className="cc-location text-2xl text-[#D1D1D1]" />
-        <div className="border-b-4 border-dotted border-[#D1D1D1] w-full"></div>
-        <i className="cc-timer text-2xl text-[#D1D1D1]" />
-      </div>
-      <span className={"font-semibold text-14"}>آدرس خود را انتخاب کنید:</span>
-      <div className={`py-4 transition-all`}>
-        <div className={"flex justify-center"}>
-          <SelectLocationTab
-            headerText={"در محل شما"}
-            addressTabState={"MOVING"}
-            selectAddressState={selectAddressState}
-            setSelectAddressState={setSelectAddressState}
-            setLocationId={setLocationId}
-          />
-          <SelectLocationTab
-            headerText={"در مراکز کارپایا"}
-            addressTabState={"FIXED"}
-            selectAddressState={selectAddressState}
-            setSelectAddressState={setSelectAddressState}
-            setLocationId={setLocationId}
-          />
-        </div>
-      </div>
-      <div className={"lg:mt-7 flex flex-col gap-5"}>
-        {
+    <div className={"relative"}>
+      <ServiceInformation
+        serviceData={[
           {
-            MOVING: (
-              <AddressSelection
-                setMyLocationData={setMyLocationData}
-                timeData={timeData}
-                myLocationData={myLocationData}
-                status={"MOVING"}
-                locationId={locationId}
-                setLocationId={setLocationId}
-              />
-            ),
-            FIXED: (
-              <AddressSelection
-                carCheckLocations={carCheckLocations}
-                status={"FIXED"}
-                timeData={timeData}
-                filter={filter}
-                locationId={locationId}
-                setLocationId={setLocationId}
-              />
-            ),
-          }[selectAddressState]
+            key: "نوع خدمات :",
+            value: batteriesCart?.batteryName,
+            icon: "cc-search",
+          },
+        ]}
+      />
+      <div
+        className={
+          "flex flex-col relative py-4 max-w-[1772px] lg:w-[calc(100%-424px)] mr-auto bg-[#FDFDFD] lg:shadow-[0_0_6px_0_rgba(125,125,125,0.5)] lg:p-6 rounded-2xl min-h-[605px] mb-4 lg:mt-7"
         }
+      >
+        <div
+          className={
+            "flex items-center gap-2 size752:gap-[16px] text-[#0E0E0E] w-full"
+          }
+        >
+          <i
+            className={"cc-arrow-right text-24 cursor-pointer"}
+            onClick={() =>
+              router.push(
+                `/services/batteries/products?attribute_slug=${attributeSlug}&attribute_value=${attributeValue}&selectTipState=${selectTipState}&amper=${amper}&type_service=${typeService}`,
+              )
+            }
+          />
+          <p className={"text-14 size752:text-16 w-full font-medium"}>
+            انتخاب مکان
+          </p>
+        </div>
+        <div
+          className={
+            "flex gap-2 items-center w-full bg-[#FFFFFF] text-[#D1D1D1] shadow-[0_0_4px_0_rgba(152,152,152,0.4)] lg:py-2 py-1 rounded-[16px] px-2 my-4"
+          }
+        >
+          <i
+            className="cc-car-o text-2xl text-[#1E67BF]"
+            onClick={() =>
+              router.push(
+                `/batteries?attribute_slug=${attributeSlug}&attribute_value=${attributeValue}&selectTipState=${selectTipState}`,
+              )
+            }
+          />
+          <div className="border-b-4 border-dotted border-[#1E67BF] w-full"></div>
+          <i
+            className="cc-search text-2xl text-[#1E67BF]"
+            onClick={() =>
+              router.push(
+                `/services/batteries/products?attribute_slug=${attributeSlug}&attribute_value=${attributeValue}&selectTipState=${selectTipState}&amper=${amper}&type_service=${typeService}`,
+              )
+            }
+          />
+          <div className="border-b-4 border-dotted border-[#1E67BF] w-full"></div>
+          <i className="cc-location text-2xl text-[#D1D1D1]" />
+          <div className="border-b-4 border-dotted border-[#D1D1D1] w-full"></div>
+          <i className="cc-timer text-2xl text-[#D1D1D1]" />
+        </div>
+        <span className={"font-semibold text-14"}>
+          آدرس خود را انتخاب کنید:
+        </span>
+        <div className={`py-4 transition-all`}>
+          <div className={"flex justify-center"}>
+            <SelectLocationTab
+              headerText={"در محل شما"}
+              addressTabState={"MOVING"}
+              selectAddressState={selectAddressState}
+              setSelectAddressState={setSelectAddressState}
+              setLocationId={setLocationId}
+            />
+            <SelectLocationTab
+              headerText={"در مراکز کارپایا"}
+              addressTabState={"FIXED"}
+              selectAddressState={selectAddressState}
+              setSelectAddressState={setSelectAddressState}
+              setLocationId={setLocationId}
+            />
+          </div>
+        </div>
+        <div className={"lg:mt-7 flex flex-col gap-5"}>
+          {
+            {
+              MOVING: (
+                <AddressSelection
+                  setMyLocationData={setMyLocationData}
+                  timeData={timeData}
+                  myLocationData={myLocationData}
+                  status={"MOVING"}
+                  locationId={locationId}
+                  setLocationId={setLocationId}
+                />
+              ),
+              FIXED: (
+                <AddressSelection
+                  carCheckLocations={carCheckLocations}
+                  status={"FIXED"}
+                  timeData={timeData}
+                  filter={filter}
+                  locationId={locationId}
+                  setLocationId={setLocationId}
+                />
+              ),
+            }[selectAddressState]
+          }
+        </div>
       </div>
     </div>
   );
