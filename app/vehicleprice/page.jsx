c635dc6cfc74d656d51b4e1f12cc06b4ef78bf87
farshad <farshad.fsn@gmail.com>
2024-10-26 13:48:45 +0330

    complete VehiclePriceModal

--- app/vehicleprice/page.jsx
+++ app/vehicleprice/page.jsx
@@ -1,57 +1,57 @@
 "use client";
 import { useEffect, useState } from "react";
-// import SelectCarAndCity from "./SelectCarAndCity";
-// import SelectCity from "./SelectCity";
-// import CarSelect from "./CarSelect";
-// import { usePathname, useSearchParams } from "next/navigation";
 import { error } from "@/utils/function-utils";
-import SelectCarAndCity from "@/components/public/SelectCarAndCity";
-import SelectCity from "@/components/public/SelectCity";
 import VehiclePriceModal from "@/components/vehiclePrice/VehiclePriceModal";
 import CarSelect from "@/components/public/CarSelect";
+import { car_parts } from "@/staticData/car_parts";
+import RadioInput from "@/components/vehiclePrice/RadioInput";
+import { postData } from "@/utils/client-api-function-utils";
 
 const vehicleprice = (props) => {
   const [client, setClient] = useState(false);
   const [asideStatus, setAsideStatus] = useState("car_city");
   const [toastieDisplay, setToastieDisplay] = useState(false);
   const [preventFirstRender, setPreventFirstRender] = useState(false);
-  // const [cityId, setCityId] = useState(null);
-  // const pathName = usePathname();
-
-  // const searchParams = useSearchParams();
-
-  // useEffect(() => {
-  //   setClient(true);
-  //   if (typeof window !== "undefined") {
-  //     const city = JSON.parse(localStorage.getItem("city"));
-  //     setCityId(city?.cityId);
-  //   }
-  // }, [toastieDisplay, searchParams, pathName]);
-
+  const [carData, setCarData] = useState({});
+  const [partsData, setPartsData] = useState({});
+  const [display, setDispaly] = useState(false);
   useEffect(() => {
     setClient(true);
     if (typeof window !== "undefined") {
       const selectedVehicle = JSON.parse(
         localStorage.getItem("selectedVehicle")
       );
-      const city = JSON.parse(localStorage.getItem("city"));
       if (preventFirstRender) {
-        if (!city) {
-          error("لطفا شهر خود را انتخاب کنید");
-        } else if (!selectedVehicle) {
+        if (!selectedVehicle) {
           error("لطفا خودرو خود را انتخاب کنید");
         }
       }
     }
   }, [preventFirstRender, toastieDisplay]);
 
+  function tabClickHandler(key, value) {
+    setPartsData({ ...partsData, [key]: value });
+  }
+
+  async function calculate() {
+    const data = {
+      ...carData,
+      model: JSON.parse(localStorage.getItem("selectedVehicle"))?.model,
+      tip: JSON.parse(localStorage.getItem("selectedVehicle"))?.title,
+      brand: JSON.parse(localStorage.getItem("selectedVehicle"))?.brand,
+      items: Object.values(partsData),
+    };
+    const res = await postData("/web/pricing/calculations", data);
+    console.log(res);
+  }
+
   if (!client) {
     return null;
   }
   return (
-    <div className="w-full min-h-screen">
+    <div className="w-full min-h-screen flex">
       <div
-        className={`lg:absolute fixed transition-all duration-500 ${props.modalClickState ? "bottom-0 right-0 left-0" : "-bottom-[100vh]"} w-full lg:top-0 lg:right-0.5 lg:h-full lg:z-0 z-[10000] flex flex-col`}
+        className={` ${display ? "hidden lg:flex" : "flex"} mt-10 lg:mt-0 lg:absolute transition-all duration-500 ${props.modalClickState ? "bottom-0 right-0 left-0" : "-bottom-[100vh]"} w-full lg:top-0 lg:right-0.5 lg:h-full lg:z-0`}
       >
         {(() => {
           switch (asideStatus) {
@@ -59,33 +59,46 @@ const vehicleprice = (props) => {
               return (
                 <VehiclePriceModal
                   buttonTitle={"تخمین قیمت"}
-                  onClick={props.onClick}
-                  // href={props.href}
-                  //   buttonTitle={"درخواست باتری"}
-                  // href={
-                  //   JSON.parse(localStorage.getItem("selectedVehicle"))?.id &&
-                  //   cityId
-                  //     ? `batteries/products?attribute_slug=type_vehicle&attribute_value=${searchParams.get("attribute_value")}${
-                  //         JSON.parse(localStorage.getItem("selectedVehicle"))?.id
-                  //           ? `&selectTipState=true,${JSON.parse(localStorage.getItem("selectedVehicle")).id.toString()}`
-                  //           : ""
-                  //       }`
-                  //     : ""
-                  // }
+                  onClick={() => setDispaly(true)}
                   setAsideStatus={setAsideStatus}
                   setToastieDisplay={setToastieDisplay}
                   setPreventFirstRender={setPreventFirstRender}
                   setModalClickState={props.setModalClickState}
+                  setCarData={setCarData}
+                  carData={carData}
+                  setDispaly={setDispaly}
                 />
               );
-            case "citySelection":
-              return <SelectCity setAsideStatus={setAsideStatus} />;
             case "carSelection":
               return <CarSelect setAsideStatus={setAsideStatus} />;
             default:
               return null;
           }
         })()}
+      </div>
+      <div
+        className={`${!display ? "hidden lg:flex" : "flex"} flex-col gap-6 w-full lg:w-[calc(100%-410px)] shadow-[0_0_6px_0_rgba(125,125,125,0.5)] mt-[52px] rounded-2xl h-fit p-4 relative bg-white mr-auto`}
+      >
+        {!display && (
+          <div className="w-full h-full bg-[#cfcfcf75] absolute top-0 right-0 rounded-2xl z-10 cursor-not-allowed hidden lg:block"></div>
+        )}
+        <div className=" grid sm:grid-cols-2 size1400:grid-cols-3 size1770:grid-cols-4 gap-4 ">
+          {car_parts.map((item) => (
+            <RadioInput
+              options={item.options}
+              name={item.persian_name}
+              tabClickHandler={tabClickHandler}
+            />
+          ))}
+        </div>
+        <button
+          className="px-4 py-2 text-[#fefefe] bg-[#F66B34] w-fit h-fit rounded-lg"
+          onClick={() => {
+            calculate();
+          }}
+        >
+          تخمین قیمت خودرو
+        </button>
       </div>
     </div>
   );
