8e3ea0f1bd7bf139c49f79004b2fa4f509899588
peyman-norozy <peyman.norozy@gmail.com>
2024-10-28 17:11:50 +0330

    change aspect in className

--- components/HomePage/HomePageMainSlider.jsx
+++ components/HomePage/HomePageMainSlider.jsx
@@ -1,7 +1,13 @@
-'use client'
-import { Autoplay, Keyboard, Mousewheel, Navigation, Pagination } from "swiper/modules";
-import { SwiperSlide ,Swiper} from "swiper/react";
-import sliderCar from "@/public/assets/images/sliderCar.png"
+"use client";
+import {
+  Autoplay,
+  Keyboard,
+  Mousewheel,
+  Navigation,
+  Pagination,
+} from "swiper/modules";
+import { SwiperSlide, Swiper } from "swiper/react";
+import sliderCar from "@/public/assets/images/sliderCar.png";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
@@ -14,51 +20,76 @@ import { API_PATHS } from "@/configs/routes.config";
 import Link from "next/link";
 
 const HomePageMainSlider = () => {
-    const [newSlider, setNewSlider] = useState([]);
-    useEffect(() => {
-        axios
-            .get(process.env.BASE_API + "/web" + API_PATHS.SLIDERS+`?type=${window.innerWidth>768?"DESKTOP":"MOBILE"}`)
-            .then((res) => {
-                // const m = res.data.data;
-                // m.push(res.data.data);
-                // m.push({});
-                setNewSlider(res.data.data);
-                // setSliderShowState(true)
-                console.log(res.data.data);
-            })
-            .catch((error) => console.log(error));
-    }, []);
-    return ( 
-        <div className="w-full home_banner_slider overflow-hidden md:rounded-[32px]">
-                <Swiper
-                cssMode={true}
-                autoplay={true}
-                pagination={{clickable: true}}
-                click
-                mousewheel={true}
-                keyboard={true}
-                modules={[Pagination, Mousewheel, Keyboard, Autoplay]}
-                className="mySwiper"
+  const [newSlider, setNewSlider] = useState([]);
+  useEffect(() => {
+    axios
+      .get(
+        process.env.BASE_API +
+          "/web" +
+          API_PATHS.SLIDERS +
+          `?type=${window.innerWidth > 768 ? "DESKTOP" : "MOBILE"}`,
+      )
+      .then((res) => {
+        // const m = res.data.data;
+        // m.push(res.data.data);
+        // m.push({});
+        setNewSlider(res.data.data);
+        // setSliderShowState(true)
+        console.log(res.data.data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+  return (
+    <div className="w-full home_banner_slider overflow-hidden md:rounded-[32px]">
+      <Swiper
+        cssMode={true}
+        autoplay={true}
+        pagination={{ clickable: true }}
+        click
+        mousewheel={true}
+        keyboard={true}
+        modules={[Pagination, Mousewheel, Keyboard, Autoplay]}
+        className="mySwiper"
+      >
+        {newSlider.map((item, index) => (
+          <SwiperSlide className="relative" key={"mainSlider" + index}>
+            <Image
+              src={
+                process.env.BASE_API +
+                "/web" +
+                API_PATHS.FILE +
+                "/" +
+                item.image_id
+              }
+              width={1600}
+              height={400}
+              className="w-full aspect-[1344/750] sm:aspect-[4/1]"
+            />
+            {item.description && (
+              <div className="w-1/2 sm:w-2/5 lg:w-1/3 absolute bg-[#5D697A52] backdrop-blur-[16px] h-fit left-4 lg:left-8 top-4 lg:top-8 flex flex-col z-[2] gap-2 lg:gap-6 p-2 sm:p-4 rounded-lg">
+                <span className="text-[#FEFEFE] text-12 lg:text-16 font-bold">
+                  {item.description}
+                </span>
+                <Link
+                  href={item.link}
+                  className="bg-[#F66B34] text-[rgb(254,254,254)] px-2 lg:px-4 py-[6px] lg:py-[8px] w-fit rounded-[6px] self-end text-[10px] lg:text-xs"
                 >
-                {newSlider.map((item ,index)=>(
-                <SwiperSlide className="relative" key={"mainSlider"+index}>
-                    <Image src={process.env.BASE_API+"/web"+API_PATHS.FILE+"/"+item.image_id} width={1344} height={480} className="w-full aspect-[1344/750] sm:aspect-[1344/485]"/>
-                    {item.description&&<div className="w-1/2 sm:w-2/5 lg:w-1/3 absolute bg-[#5D697A52] backdrop-blur-[16px] h-fit left-4 lg:left-8 top-4 lg:top-8 flex flex-col z-[2] gap-2 lg:gap-6 p-2 sm:p-4 rounded-lg">
-                        <span className="text-[#FEFEFE] text-12 lg:text-16 font-bold">{item.description}</span>
-                        <Link href={item.link} className="bg-[#F66B34] text-[rgb(254,254,254)] px-2 lg:px-4 py-[6px] lg:py-[8px] w-fit rounded-[6px] self-end text-[10px] lg:text-xs">مشاهده بیشتر</Link>
-                    </div>}
-                </SwiperSlide> 
-                ))}
-                {/* <SwiperSlide className="relative">
+                  مشاهده بیشتر
+                </Link>
+              </div>
+            )}
+          </SwiperSlide>
+        ))}
+        {/* <SwiperSlide className="relative">
                     <Image src={sliderCar} className="w-full aspect-[1344/574]"/>
                     <div className="w-1/2 sm:w-2/5 lg:w-1/3 absolute bg-[#5D697A52] backdrop-blur-[16px] h-fit left-4 lg:left-8 top-4 lg:top-8 flex flex-col z-[2] gap-2 lg:gap-6 p-2 sm:p-4">
                         <span className="text-[#FEFEFE] text-12 lg:text-16 font-bold">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.</span>
                         <button className="bg-[#F66B34] text-[rgb(254,254,254)] px-2 lg:px-4 py-[6px] lg:py-[8px] w-fit rounded-[6px] self-end text-[10px] lg:text-xs">مشاهده بیشتر</button>
                     </div>
                 </SwiperSlide>  */}
-            </Swiper>
-        </div>
-     );
-}
- 
+      </Swiper>
+    </div>
+  );
+};
+
 export default HomePageMainSlider;
