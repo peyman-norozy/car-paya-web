540f6f0fbcd3f2e15bee3d4f094a6542ef0a04c1
farshad <farshad.fsn@gmail.com>
2024-11-16 15:12:52 +0330

    create PanelContainer component

---
+++ app/panel/dashboard/page.jsx
@@ -0,0 +1,11 @@
+import PanelContainer from "@/layouts/PanelContainer";
+
+const Dashboard = () => {
+  return (
+    <PanelContainer>
+      <div className="bg-slate-500 h-[800px]"></div>
+    </PanelContainer>
+  );
+};
+
+export default Dashboard;
