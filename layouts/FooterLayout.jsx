967b07aebe92121017e8d9afa312ac122841ca59
farshad <farshad.fsn@gmail.com>
2024-11-20 16:20:43 +0330

    fix footer condition

--- layouts/FooterLayout.jsx
+++ layouts/FooterLayout.jsx
@@ -23,14 +23,7 @@ const Footer = (props) => {
   );
 
   useEffect(() => {
-    if (
-      pathName.includes("/services/vehicle-inspection") &&
-      searchParams.toString().includes("step=")
-    ) {
-      setFooterState(false);
-    } else if (pathName.startsWith("/services/batteries/")) {
-      setFooterState(false);
-    } else if (pathName.startsWith("/services/detailing/")) {
+    if (pathName.startsWith("/services/") || pathName.startsWith("/panel")) {
       setFooterState(false);
     } else {
       setFooterState(true);
