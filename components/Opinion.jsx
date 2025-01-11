86f19a3f0ea8c9b5e332256ec282f2b57cbb2dc3
farshad <farshad.fsn@gmail.com>
2025-01-11 10:57:34 +0330

    fix Opinion style

--- components/Opinion.jsx
+++ components/Opinion.jsx
@@ -88,8 +88,7 @@ const Opinion = () => {
         modules={[Pagination]}
         style={{
           width: "100%",
-          paddingRight: "4px",
-          paddingLeft: "4px",
+          padding: "4px",
         }}
         // centeredSlides={true}
         // loop={true}
@@ -98,19 +97,23 @@ const Opinion = () => {
           <SwiperSlide style={{ width: "fit-content" }} key={item.id}>
             <div className="w-[242px] h-[154px] sm:w-[359px]  sm:h-[217px] rounded-2xl md:rounded-[32px] overflow-hidden flex flex-col items-center gap-[10px] bg-[#FFFFFF] shadow-[0_0_8px_0_rgba(162,162,162,0.25)] lg:py-[34px] py-[20px] px-3 sm:p-4">
               <div className={"flex justify-between w-full"}>
-                <span className={"text-[#0F0F0F] font-bold lg-text-16 text-14"}>
+                <span className={"text-[#0F0F0F] font-bold lg:text-16 text-14"}>
                   {item.name}
                 </span>
                 <div className={"flex items-center gap-2"}>
-                  <span className={"lg:text-16 text-12 font-medium"}>4.2</span>
+                  <span className={"lg:text-16 text-12 lg:font-medium"}>
+                    4.2
+                  </span>
                   <i className={"cc-star text-[#FDB022] text-20"} />
                 </div>
               </div>
-              <p className={"lg:text-16 text-12"}>{item.description}</p>
-              <div className={"w-full flex justify-end gap-2"}>
+              <p className={"lg:text-16 text-12 lg:font-medium"}>
+                {item.description}
+              </p>
+              {/* <div className={"w-full flex justify-end gap-2"}>
                 <span className={"lg:text-16 text-12"}>تاریخ:</span>
                 <span className={"lg:text-16 text-12"}>{item.date}</span>
-              </div>
+              </div> */}
             </div>
           </SwiperSlide>
         ))}
